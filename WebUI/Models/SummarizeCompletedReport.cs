8706fd141360a99b9532f6222553227cc142892e
Marc Durham <marcdurham@gmail.com>
2019-09-15 12:37:03 -0700

    Add summary report

---
+++ WebUI/Models/SummarizeCompletedReport.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AlbaClient.Controllers.UseCases;
+using Controllers.UseCases;
+
+namespace WebUI
+{
+    public class SummarizeCompletedReport
+    {
+        public int CompletedCount { get; set; } 
+    }
+}
