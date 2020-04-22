e6436a134d90f028e0e58f68455d1c456458203d
Marc Durham <marcdurham@gmail.com>
2020-04-22 15:12:29 -0700

    Add new columns

--- Data/Migrations/20200422221202_AddIPAndDate.Designer.cs
+++ Data/Migrations/20200422221202_AddIPAndDate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebUI.Areas.Identity.Data;
 
 namespace WebUI.Migrations
 {
     [DbContext(typeof(MainDbContext))]
-    partial class MainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200422221202_AddIPAndDate")]
+    partial class AddIPAndDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
