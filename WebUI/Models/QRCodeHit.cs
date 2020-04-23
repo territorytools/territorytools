8403507ec9c3882c9cbe4b53e338b0bca0244cc0
Marc Durham <marcdurham@gmail.com>
2020-04-23 00:09:15 -0700

    Fix warnings

---
+++ WebUI/Models/QRCodeHit.cs
@@ -0,0 +1,11 @@
+﻿namespace WebUI
+{
+    public class QRCodeHit
+    {
+        public string Time { get; set; }
+        public string ShortUrl { get; set; }
+        public string Subject { get; set; }
+        public string Note { get; set; }
+       
+    }
+}
