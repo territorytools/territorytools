a578cb970a2ec7c78b7a10b223d21c166e743df3
Marc Durham <marcdurham@gmail.com>
2020-04-23 08:27:14 +0000

    Update UI

--- WebUI/Models/QRCodeHit.cs
+++ WebUI/Models/QRCodeHit.cs
@@ -4,8 +4,12 @@
     {
         //public string Time { get; set; }
         public string ShortUrl { get; set; }
+        public string OriginalUrl { get; set; }
+        public string Created { get; set; }
+        public string HitCount { get; set; }
+        public string LastIPAddress { get; set; }
+        public string LastTimeStamp { get; set; }
         public string Subject { get; set; }
         public string Note { get; set; }
-       
     }
 }
