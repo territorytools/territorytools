fc1e747d1948556a432b26410b40e095951c8f9d
Marc Durham <marcdurham@gmail.com>
2020-05-04 07:46:38 +0000

    Save the tick file

--- territory-tools-web-clock.sh
+++ territory-tools-web-clock.sh
@@ -1,1 +1,2 @@
-curl https://calendar.google.com/calendar/ical/projects.xfire%40gmail.com/private-7d1e2e46a9569adb03f9ff678b033d9f/basic.ics > /var/www/x-fire.do.md9.us/samples/basic.ics
+curl https://territorytools.org/ClockTick
+curl https://test.territorytools.org/ClockTick
