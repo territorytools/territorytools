bc2838e764b3976fd01b6a419a138eb8cd58577c
Marc Durham <v-madurh@microsoft.com>
2020-05-05 00:28:09 -0700

    Pull client id and secret in as args
    Getting ready to write a secret

--- TerritoryShell/Program.cs
+++ TerritoryShell/Program.cs
@@ -7,7 +7,7 @@ namespace TerritoryShell
         static void Main(string[] args)
         {
             Console.WriteLine("Getting a vault secret....");
-            string secret = VaultClient.GetSecret();
+            string secret = VaultClient.GetSecret(args[0], args[1]);
             Console.WriteLine($"Secret: {secret}");
             Console.WriteLine("Press any key to quit...");
             Console.ReadKey();
--- TerritoryShell/VaultClient.cs
+++ TerritoryShell/VaultClient.cs
@@ -11,21 +11,24 @@ namespace TerritoryShell
 {
     public class VaultClient
     {
-        const string CLIENTSECRET = "xxxxxxx";
-        const string CLIENTID = "xxxxxxx";
         const string BASESECRETURI = "https://territorywebvault.vault.azure.net";
         static KeyVaultClient keyVaultClient = null;
+        static string _clientId;
+        static string _clientSecret;
 
-        public static string GetSecret()
+        public static string GetSecret(string clientId, string clientSecret)
         {
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+
             var client = new VaultClient();
-            Task<string> task = VaultClient.GetSecretFrom();
+            Task<string> task = VaultClient.GetSecretFrom(clientId, clientSecret);
             
             string message = task.Result;
             
             return message;
         }
-        public static async Task<string> GetSecretFrom()
+        public static async Task<string> GetSecretFrom(string clientId, string clientSecret)
         {
             // Resources
             // https://www.codeproject.com/Tips/1430794/Using-Csharp-NET-to-Read-and-Write-from-Azure-Key
@@ -47,7 +50,7 @@ namespace TerritoryShell
                 keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetToken));
 
                 SecretBundle secret = Task.Run(() => keyVaultClient.GetSecretAsync(BASESECRETURI +
-                    @"/secrets/" + "TestUserName")).ConfigureAwait(false).GetAwaiter().GetResult();
+                    @"/secrets/" + "alba-account-user-seattleeastchinese")).ConfigureAwait(false).GetAwaiter().GetResult();
 
                 return secret.Value;
             }
@@ -58,10 +61,42 @@ namespace TerritoryShell
             }
         }
 
-        public static async Task<string> GetToken(string authority, string resource, string scope)
+        private static async void WriteKeyVault(string name, string value)
+        {
+            var attribs = new SecretAttributes
+            {
+                Enabled = true//,
+                //Expires = DateTime.UtcNow.AddYears(2), // if you want to expire the info
+                //NotBefore = DateTime.UtcNow.AddDays(1) // if you want the info to 
+                // start being available later
+            };
+
+            IDictionary<string, string> alltags = new Dictionary<string, string>();
+            //alltags.Add("Test1", "This is a test1 value");
+            //alltags.Add("Test2", "This is a test2 value");
+            //alltags.Add("CanBeAnything", "Including a long encrypted string if you choose");
+            //string TestName = "TestSecret";
+            //string TestValue = "searchValue"; // this is what you will use to search for the item later
+            string contentType = "SecretInfo"; // whatever you want to categorize it by; you name it
+
+            SecretBundle bundle = await keyVaultClient.SetSecretAsync(
+                vaultBaseUrl: BASESECRETURI,
+                secretName: name,
+                value: value,
+                tags: alltags,
+                contentType: contentType,
+                secretAttributes: attribs); ;
+
+            Console.WriteLine($"Secret written to key vault: {name}");
+        }
+
+        public static async Task<string> GetToken(
+            string authority, 
+            string resource, 
+            string scope)
         {
             var authContext = new AuthenticationContext(authority);
-            ClientCredential clientCred = new ClientCredential(CLIENTID, CLIENTSECRET);
+            var clientCred = new ClientCredential(_clientId, _clientSecret);
             AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
 
             if (result == null)
