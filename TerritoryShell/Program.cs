cf3fb556124c8a81b770855deabfd0370b2ea267
Marc Durham <marcdurham@gmail.com>
2020-05-05 22:22:07 -0700

    Clean up console code

--- TerritoryShell/Program.cs
+++ TerritoryShell/Program.cs
@@ -4,6 +4,8 @@ namespace TerritoryShell
 {
     class Program
     {
+        const string VaultName = "TerritoryWebVault";
+
         static void Main(string[] args)
         {
             string clientId = args[0];
@@ -13,16 +15,25 @@ namespace TerritoryShell
             if (args.Length == 3)
             {
                 Console.WriteLine($"Getting a vault secret for {name}....");
-                string secret = AzureKeyVaultClient.GetSecret(clientId, clientSecret, name);
+                var client = new AzureKeyVaultClient(
+                    clientId,
+                    clientSecret,
+                    VaultName);
+
+                string secret = client.GetSecret(name);
 
                 Console.WriteLine($"Secret: {secret}");
             }
             else if (args.Length == 4)
             {
                 Console.WriteLine($"Writing a vault secret for {name}....");
-                
+                var client = new AzureKeyVaultClient(
+                    clientId,
+                    clientSecret,
+                    VaultName);
+
                 string value = args[3];
-                AzureKeyVaultClient.WriteSecret(clientId, clientSecret, name, value);
+                client.WriteSecret(name, value);
             }
 
             Console.WriteLine("Press any key to quit...");
