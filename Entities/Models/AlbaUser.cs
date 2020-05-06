cde3803a00be62422d2dc85bf070d90e2c3a370a
Marc Durham <marcdurham@gmail.com>
2020-05-05 23:04:46 -0700

    Add database entities for Alba

--- Entities/Models/AlbaUser.cs
+++ Entities/Models/AlbaUser.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebUI.Models
 {
@@ -10,6 +9,7 @@ namespace WebUI.Models
         public int Id { get; set; }
         public string UserName { get; set; }
         public int AccountId { get; set; }
+        public AlbaAccount Account { get; set; }
         public string Email { get; set; }
         public string Telephone { get; set; }
         public string Name { get; set; }
