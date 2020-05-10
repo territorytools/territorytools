864f44722f1442276970e33d65cdc576b1df7bb0
Marc Durham <marcdurham@gmail.com>
2020-05-10 15:41:37 -0700

    Make QRCodes editable

---
+++ WebUI/Areas/UrlShortener/Models/ShortUrlShow.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.Areas.UrlShortener.Models
+{
+    public class ShortUrlShow
+    {
+        public int Id { get; set; }
+
+        public string ShortUrl { get; set; }
+
+        public string Subject { get; set; }
+
+        public string LetterLink { get; set; }
+
+        public string Note { get; set; }
+        public string OriginalUrl { get; internal set; }
+    }
+}
