8748bb2fc695c681b4272fd958bf285a51e73361
Marc Durham <marcdurham@gmail.com>
2020-05-17 18:19:47 -0700

    Remove copied code

--- Alba/Controllers/AlbaServer/AddressViewPageResultParser.cs
+++ Alba/Controllers/AlbaServer/AddressViewPageResultParser.cs
@@ -7,7 +7,7 @@ namespace AlbaClient.Controllers.AlbaServer
 {
     public class AddressView
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Group { get; set; }
         public string TerritoryDescriptionOrStatus { get; set; }
         public string Status { get; set; }
@@ -54,76 +54,9 @@ namespace AlbaClient.Controllers.AlbaServer
         {
             var users = new List<AddressView>();
 
-            var doc = new HtmlDocument();
-            doc.LoadHtml(html);
+            // TODO: Copy one of the other HTML parsers
 
-            var rowNodes = doc.DocumentNode.SelectNodes("//tr");
-
-            if (rowNodes != null)
-            {
-                foreach (HtmlNode rowNode in rowNodes)
-                {
-                    string userIdString = rowNode.GetAttributeValue("id", string.Empty);
-
-                    if (!string.IsNullOrWhiteSpace(userIdString))
-                    {
-                        userIdString = userIdString.Replace("us", string.Empty);
-                    }
-
-                    int.TryParse(userIdString, out int userId);
-
-                    var user = new AddressView
-                    {
-                        Id = userId
-                    };
-
-                    var colNodes = rowNode.SelectNodes("td");
-
-                    if (colNodes != null)
-                    {
-                        for (int col = 0; col < colNodes.Count; col++)
-                        {
-                            HtmlNode colNode = colNodes[col];
-                            switch (col)
-                            {
-                                case 0:
-                                    // User ID is already parsed from attribute
-                                    break;
-                                case 1:
-                                    user.UserName = colNode.SelectSingleNode("strong").InnerText;
-                                    break;
-                                case 2:
-                                    user.Name = colNode.InnerText;
-                                    break;
-                                case 3:
-                                    user.Email = colNode.InnerText;
-                                    break;
-                                case 4:
-                                    user.Telephone = colNode.InnerText;
-                                    break;
-                                case 5:
-                                    // Password cannot be downloaded (and should not)
-                                    break;
-                                case 6:
-                                    user.Role = colNode.SelectSingleNode("span").InnerText;
-                                    break;
-                                case 7:
-                                    // TODO: Add later
-                                    //user.Created = colNode.InnerText;
-                                    break;
-                                case 8:
-                                    // user.LastSignIn = colNode.InnerText;
-                                    // IP Address
-                                    break;
-                            }
-                        }
-
-                        users.Add(user);
-                    }
-                }
-            }
-
-            return users;
+            throw new NotImplementedException();
         }
     }
 }
