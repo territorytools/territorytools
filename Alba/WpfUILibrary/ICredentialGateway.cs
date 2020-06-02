952eed9840097541e1731af8df71fd22fda5beee
Marc Durham <marcdurham@gmail.com>
2020-06-01 22:46:28 -0700

    Add missing interface

---
+++ Alba/WpfUILibrary/ICredentialGateway.cs
@@ -0,0 +1,11 @@
+﻿namespace AlbaClient.WpfUILibrary
+{
+    public interface ICredentialGateway
+    {
+        string AccountName { get; set; }
+        string UserName { get; set; }
+        string GetKeyValue(string v);
+        void Save();
+        void SetKeyValue(string v, string password);
+    }
+}
