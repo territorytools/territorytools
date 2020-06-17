2e2cda96fc5134310caeb7e75313ea44b25ed382
Marc Durham <marcdurham@gmail.com>
2020-06-16 22:01:08 -0700

    Add migration for host name

--- Data/Migrations/20200617050001_AddAlbaHostName.Designer.cs
+++ Data/Migrations/20200617050001_AddAlbaHostName.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebUI.Areas.Identity.Data;
 
 namespace WebUI.Migrations
 {
     [DbContext(typeof(MainDbContext))]
-    partial class MainDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200617050001_AddAlbaHostName")]
+    partial class AddAlbaHostName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20200617050001_AddAlbaHostName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebUI.Migrations
+{
+    public partial class AddAlbaHostName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HostName",
+                table: "AlbaAccounts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HostName",
+                table: "AlbaAccounts");
+        }
+    }
+}
