a36521e3b3478f5abd9288fd744850afbeb267d6
Marc Durham <marcdurham@gmail.com>
2020-06-20 21:14:32 -0700

    Add migration for host

---
+++ Web/Data/Migrations/20200621041408_AddUrlHost.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebUI.Migrations
+{
+    public partial class AddUrlHost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HostId",
+                table: "ShortUrls",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Path",
+                table: "ShortUrls",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "ShortUrlHosts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    AllowNewUrls = table.Column<bool>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShortUrlHosts", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortUrls_HostId",
+                table: "ShortUrls",
+                column: "HostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "ForeignKey_ShortUrl_Host",
+                table: "ShortUrls",
+                column: "HostId",
+                principalTable: "ShortUrlHosts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "ForeignKey_ShortUrl_Host",
+                table: "ShortUrls");
+
+            migrationBuilder.DropTable(
+                name: "ShortUrlHosts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShortUrls_HostId",
+                table: "ShortUrls");
+
+            migrationBuilder.DropColumn(
+                name: "HostId",
+                table: "ShortUrls");
+
+            migrationBuilder.DropColumn(
+                name: "Path",
+                table: "ShortUrls");
+        }
+    }
+}
