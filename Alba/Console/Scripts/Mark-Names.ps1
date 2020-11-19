3b18bd8931632dc433eeeaec47b514842982369c
Marc Durham <marcdurham@gmail.com>
2020-11-19 15:03:07 -0800

    Update name marking script

--- Alba/Console/Scripts/Mark-Names.ps1
+++ Alba/Console/Scripts/Mark-Names.ps1
@@ -2,35 +2,51 @@
    [Parameter(Mandatory)]
    [string]$nameFile,
    [Parameter(Mandatory)]
-   [string]$rowFile,
+   [string]$sourceFile,
+   [Parameter()]
+   [string]$markedOutputFile = "$sourceFile.marked.txt",
+   [Parameter()]
+   [string]$extractedOutputFile = "$sourceFile.extracted.txt",
    [Parameter()]
    [int]$indexOfNameColumn = 0,
    [Parameter()]
    [string]$markWith = "***"
 )
 
-####################################################################
-"Mark rows with targeted names, in the same format as the input file"
-####################################################################
+#################################################################################
+"Mark rows with matched names, output in the same format as the input file"
+#################################################################################
 
-$names = Get-Content $nameFile ; 
-$rows = Get-Content $rowFile ;
+$names = Get-Content $nameFile 
+$rows = Get-Content $sourceFile
 
-$all = @();
-ForEach ( $row in $rows ) { 
-    Foreach( $name in $names ) { 
-        $f=$false; 
-        $namesInRow = $row.Split("`t")[$indexOfNameColumn].ToUpper().Split(" ")
+$all = @()
+$marked = @()
+ForEach($row in $rows) { 
+    ForEach($name in $names) { 
+        $f=$false
+        $nameColumn = $row.Split("`t")[$indexOfNameColumn]
+        $cleaned = $nameColumn.ToUpper().Replace("+"," ").Replace(";"," ").Replace(","," ").Replace("&"," ")
+        $namesInRow = $cleaned.Split(" ")
         if($namesInRow.Contains($name.ToUpper())) { 
-            Write-Host -ForegroundColor Green "$row ($name)"; 
-            $all += "$markWith$row"; 
-            $f=$true; 
-            break; 
+            #Write-Host -ForegroundColor Green "$row ($name)"; 
+            $all += "$markWith$row"
+            $marked += "$row"
+            $markCount++
+            $f=$true
+            break
         } 
     } 
     if(!$f) { 
-        Write-Host -ForegroundColor Red "$row" 
-        $all += "$row"; 
+        #Write-Host -ForegroundColor Red "$row" 
+        $all += "$row"
     } 
-} ; 
-$all > "$rowFile(marked).txt"
+} 
+
+$all > $outputFile
+$marked > $extractedOutputFile
+
+Write-Host "$($names.Count) names in NameFile: $nameFile"
+Write-Host "$($rows.Count) rows in original SourceFile: $sourceFile"
+Write-Host "$($marked.Count) rows marked in MarkedOutputFile: $markedOutputFile"
+Write-Host "$($marked.Count) rows extracted into ExtractedOutputFile: $extractedOutputFile"
