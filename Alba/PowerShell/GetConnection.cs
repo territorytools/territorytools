fcda6a94ef09c0fe11c311efbfc91e3184a4edcc
Marc Durham <marcdurham@gmail.com>
2020-12-06 10:32:53 -0800

    Add PowerShell Cmdlets for uploading

---
+++ Alba/PowerShell/GetConnection.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Management.Automation;
+using TerritoryTools.Alba.Controllers;
+using TerritoryTools.Alba.Controllers.AlbaServer;
+using TerritoryTools.Alba.Controllers.Models;
+
+namespace PowerShell
+{
+    [Cmdlet(VerbsCommon.Get,"Connection")]
+    [OutputType(typeof(AuthorizationClient))]
+    public class GetConnection : PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string AlbaHost { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string Account { get; set; }
+        
+        [Parameter(Mandatory = true)]
+        public string User { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string Password { get; set; }
+
+        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
+        protected override void BeginProcessing()
+        {
+          
+        }
+
+        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
+        protected override void ProcessRecord()
+        {
+
+        }
+
+        // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
+        protected override void EndProcessing()
+        {
+            try
+            {
+                WriteObject(GetClient());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public AuthorizationClient GetClient()
+        {
+            try
+            {
+                var client = new AuthorizationClient(
+                    new CookieWebClient(),
+                    new ApplicationBasePath("https://", AlbaHost, "/alba"));
+
+                var creds = new Credentials(Account, User, Password);
+
+                client.Authenticate(creds);
+
+                return client;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Alba/PowerShell/SendAddress.cs
@@ -0,0 +1,84 @@
+﻿using Controllers.AlbaServer;
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using TerritoryTools.Alba.Controllers.AlbaServer;
+using TerritoryTools.Alba.Controllers.UseCases;
+
+namespace PowerShell
+{
+    [Cmdlet("Send","Address")]
+    [OutputType(typeof(DuplicatedAddress))]
+    public class SendAddress : PSCmdlet
+    {
+        List<string> errors = new List<string>();
+
+        [Parameter]
+        public string LanguageFilePath { get; set; }
+
+        [Parameter]
+        public string AddressFilePath { get; set; }
+
+        [Parameter]
+        public AuthorizationClient Connection { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public AlbaAddressImport Address { get; set; }
+
+        [Parameter]
+        public int UploadDelayMs { get; set; } = 300;
+
+        // This method gets called once for each cmdlet in the pipeline when the pipeline starts executing
+        protected override void BeginProcessing()
+        {
+            Import();
+        }
+
+        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
+        protected override void ProcessRecord()
+        {
+            try
+            {
+          
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+
+        // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
+        protected override void EndProcessing()
+        {
+            WriteVerbose("ERRORS:");
+            foreach(string error in errors)
+            {
+                WriteVerbose(error);
+            }
+        }
+
+        public void Import()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(LanguageFilePath)
+                    || string.IsNullOrWhiteSpace(AddressFilePath))
+                {
+                    return;
+                }
+
+                new ImportAddress(Connection, UploadDelayMs)
+                    .Upload(AddressFilePath, LanguageFilePath);
+            }
+            catch (UserException)
+            {
+                //view.ShowMessageBox(e.Message);
+                //WriteError(e.Message);
+            }
+        }
+    }
+}
