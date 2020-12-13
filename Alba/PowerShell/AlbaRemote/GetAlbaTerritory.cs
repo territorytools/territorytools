84936fcfd19d2650385b942973a48455ea2dd9cd
Marc Durham <marcdurham@gmail.com>
2020-12-12 19:38:53 -0800

    Add more Cmdlets

---
+++ Alba/PowerShell/AlbaRemote/GetAlbaTerritory.cs
@@ -0,0 +1,36 @@
+﻿using Controllers.UseCases;
+using System;
+using System.Management.Automation;
+using TerritoryTools.Alba.Controllers.AlbaServer;
+
+namespace TerritoryTools.Alba.PowerShell
+{
+    [Cmdlet(VerbsCommon.Get, "AlbaTerritory")]
+    [OutputType(typeof(AlbaHtmlUser))]
+    public class GetAlbaTerritory : PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public AlbaConnection Connection { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                var resultString = Connection.DownloadString(
+                    RelativeUrlBuilder.GetAllTerritories());
+
+                var territories = TerritoryResultParser.Parse(resultString);
+
+                foreach (var territory in territories)
+                {
+                    WriteObject(territory);
+                }
+            }
+            catch(Exception e)
+            {
+                WriteError(
+                    new ErrorRecord(e, "1", ErrorCategory.NotSpecified, null));
+            }
+        }
+    }
+}
