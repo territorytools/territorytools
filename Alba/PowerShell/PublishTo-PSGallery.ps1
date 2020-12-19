3184298ed8e7414f9df21489a8c9bcbcf5017d39
Marc Durham <marcdurham@gmail.com>
2020-12-19 00:06:30 -0800

    Add publish script

---
+++ Alba/PowerShell/PublishTo-PSGallery.ps1
@@ -0,0 +1,7 @@
+﻿$folderPath = "$PSScriptRoot\publish\TerritoryTools.Alba.PowerShell"
+$timestamp = (Get-Date -Format "yyyy-MM-dd-HHmmss")
+
+Publish-Module `
+  -Path $folderPath  `
+  -NuGetApiKey $env:PSGALLERY_NUGETAPIKEY `
+  -Repository PSGallery 
