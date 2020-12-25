fdc376f317a236d9b0b6f9707df5c581f984a604
Marc Durham <marcdurham@gmail.com>
2020-12-25 10:57:09 -0800

    Add & update example scripts

--- Alba/PowerShell/ExampleScripts/SignedOutReport2.ps1
+++ Alba/PowerShell/ExampleScripts/SignedOutReport2.ps1
@@ -12,7 +12,7 @@ $territories `
         -Fragment `
         -PreContent "<h1>$($_.Name)</h1>" `
         -PostContent "<p>$($_.Group.Count) Territories</p>" `
-      | % { $_ -Replace "<td>(https://[^< ]*?)</td>", "<td><button href=`"`$1`">link</button></td>" } `
+      | % { $_ -Replace "<td>(https://[^< ]*?)</td>", "<td><a href=`"`$1`">link</a></td>" } `
       > "$HOME\Desktop\TerritoriesHtml\$($_.Name).html"; `
     $group = $_.Group | Sort MonthsSignedOut; `
     }
