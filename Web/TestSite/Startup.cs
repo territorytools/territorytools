6698a6b1c214da120468cb664f72d2fe7e7a45f6
Marc Durham <marcdurham@gmail.com>
2020-12-25 17:10:24 -0800

    Fix ending brace

--- Web/TestSite/Startup.cs
+++ Web/TestSite/Startup.cs
@@ -101,7 +101,7 @@ namespace TestSite
             {
                 var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
                 // TODO: Use your User Agent library of choice here.
-                if (false) /* UserAgent doesn’t support new behavior */)
+                if (false) /* UserAgent doesn’t support new behavior */
                 {
                     // For .NET Core < 3.1 set SameSite = (SameSiteMode)(-1)
                     options.SameSite = SameSiteMode.Unspecified;
