e22945e51f6d77caa3c6447078f3b30ea4b5081a
Marc Durham <marcdurham@gmail.com>
2020-12-27 00:42:01 -0800

    Add a handy Cmdlet for testing URIs

---
+++ Alba/PowerShell/AlbaRemote/InvokeAlbaWebRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace TerritoryTools.Alba.PowerShell
+{
+    [Cmdlet("Invoke", "AlbaWebRequest")]
+    public class InvokeAlbaWebRequest : AlbaConnectedCmdlet
+    {
+        [Parameter(
+           Mandatory = true,
+           Position = 0,
+           ValueFromPipeline = true,
+           ValueFromPipelineByPropertyName = true)]
+        public string RelativeUri { get; set; }
+
+        [Parameter]
+        public SwitchParameter Force { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                string result = Connection.DownloadString(RelativeUri);
+
+                WriteObject(result);
+            }
+            catch(Exception e)
+            {
+                WriteError(new ErrorRecord(e, "1", ErrorCategory.NotSpecified, null));
+            }
+        }
+    }
+}
