e01dcc92fc4242a380a9a68586f1368ef73aa840
Marc Durham <marcdurham@gmail.com>
2021-02-14 12:04:08 -0800

    Improve signed out report

--- Alba/PowerShell/ExampleScripts/SignedOutReport2.ps1
+++ Alba/PowerShell/ExampleScripts/SignedOutReport2.ps1
@@ -1,11 +1,20 @@
-﻿mkdir "$HOME\Desktop\TerritoriesHtml" -ErrorAction SilentlyContinue
+﻿$folderPath = "$HOME\Desktop\TerritoriesHtml" 
+mkdir $folderPath -ErrorAction SilentlyContinue
 $users = Get-AlbaUser
+$userMap = @{}
+ForEach($user in $users) {
+  $userMap[$user.Name] = $user
+}
+$names = @("Marc Durham", "Some Otherguy")  
 $territories = Get-AlbaTerritory
 $territories |
   Where Status -eq Signed-out |
+  Where {$names -Contains $_.SignedOutTo } |
   Group -Property SignedOutTo |
   % { 
-      $email = $users | Where Name -eq $_.Name | Select -ExpandProperty Email
+      $email = $users |
+        Where Name -eq $_.Name |
+        Select -ExpandProperty Email
       $html = $_.Group |
         Sort SignedOut |
         Select Number, 
@@ -17,8 +26,15 @@ $territories |
             -PreContent "<p>Hello, this is an automatic email sent from your territory system.  This is a report of all the territories we have checked out under your name.  If they are completed just click the provided link and mark them as completed. Please just reply to this email if any of them are wrong.</p>" `
             -PostContent "<p>$($_.Group.Count) Territories</p>" |
           ForEach { $_ -Replace "<td>(https://[^< ]*?)</td>", "<td><a href=`"`$1`">link</a></td>" } 
-        $html | Out-File "$HOME\Desktop\TerritoriesHtml\$($_.Name).html" -Encoding utf8 
-      Send-SendGridMail -From "Your Territory System <auto@territorytools.org>" -To some@email.com -Subject "You Have $($_.Group.Count) Territories Checked Out" -Message "$html" -ContentType text/html
+      $html | Out-File "$folderPath\$($_.Name).html" -Encoding utf8       
+      $emailParams = @{
+        From = "Your Territory System <auto@territorytools.org>"
+        To = $userMap[$_.Name].EMail
+        Subject = "You Have $($_.Group.Count) Territories Checked Out" 
+        Message = "$html" 
+        ContentType = "text/html"
+      }
+      Send-SendGridMail @emailParams
     }
 
    
