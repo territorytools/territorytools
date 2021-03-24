a9fd3115f29e692d227dd24facb533205cbd5287
Marc Durham <marcdurham@gmail.com>
2021-03-23 22:02:19 -0700

    Delete a couple properties

--- Web/MainSite/Services/AuthMessageSenderOptions.cs
+++ Web/MainSite/Services/AuthMessageSenderOptions.cs
@@ -7,7 +7,5 @@
         public string AuthEmailFromEmailAddress { get; set; }
         public string AuthEmailFromName { get; set; }
         public string AuthEmailTo { get; set; }
-        public string PurchaseManager { get; set; }
-        public string BuyerEmail { get; set; }
     }
 }
