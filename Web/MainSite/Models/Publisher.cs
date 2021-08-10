1cb89bc9290802e0fbd3f36c0624df90ee5f5000
Marc Durham <marcdurham@gmail.com>
2021-08-10 11:30:04 -0700

    Show two menu buttons on the home page

--- Web/MainSite/Models/Publisher.cs
+++ Web/MainSite/Models/Publisher.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using TerritoryTools.Alba.Controllers.UseCases;
-using TerritoryTools.Web.MainSite.Services;
 
 namespace TerritoryTools.Web.MainSite.Models
 {
@@ -8,6 +7,7 @@ namespace TerritoryTools.Web.MainSite.Models
     {
         public string Name { get; set; }
         public string Email { get; set; }
+        public bool IsAdmin { get; set; }
         public List<AlbaAssignmentValues> Territories { get; set; } 
             = new List<AlbaAssignmentValues>();
         public List<QRCodeHit> QRCodeActivity { get; set; }
