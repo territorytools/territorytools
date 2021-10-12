f82669e2c635573df65f07005eb1d33359dd9be2
Marc Durham <marcdurham@gmail.com>
2021-10-11 20:55:19 -0700

    Start creating back to S-13 feature

---
+++ Alba/Controllers/AlbaBackupToS13/AssignmentChange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TerritoryTools.Alba.Controllers.AlbaBackupToS13
+{
+    public class AssignmentChange
+    {
+        public DateTime TimeStamp { get; set; }
+        public DateTime Date { get; set; }
+        public string TerritoryNumber { get; set; }
+        public string Publisher { get; set; }
+        public AssignmentStatus Status { get; set; }
+        public string Note { get; set; }
+    }
+}
