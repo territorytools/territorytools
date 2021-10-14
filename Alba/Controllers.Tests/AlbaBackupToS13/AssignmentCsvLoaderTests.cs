657b28de79ba063d8ecc716d8f630d0e024457a4
Marc Durham <marcdurham@gmail.com>
2021-10-14 01:23:18 -0700

    Add test folders

--- Alba/Controllers.Tests/AlbaBackupToS13/AssignmentCsvLoaderTests.cs
+++ Alba/Controllers.Tests/AlbaBackupToS13/AssignmentCsvLoaderTests.cs
@@ -24,30 +24,32 @@ namespace TerritoryTools.Alba.Controllers.Tests.AlbaBackupToS13
         }
 
         [Test]
-        public void LoadTestFile_OK()
+        public void LoadTestFileInFolder_OK()
         {
-            var result = AssignmentCsvLoader.LoadFromCsv("AlbaBackupToS13/territories.txt");
+            var result = AssignmentCsvLoader.LoadFromCsv("AlbaBackupToS13/2004-04-04_125959/territories.txt");
 
             Assert.That(result != null);
             Assert.AreEqual(3, result.Count, "Result count");
 
-            Assert.AreEqual("1001", result[0].Number);
-            Assert.AreEqual("", result[0].SignedOutTo);
-            Assert.IsNull(result[0].SignedOut);
-            Assert.AreEqual("Bruce Wayne", result[0].LastCompletedBy);
-            Assert.AreEqual(DateTime.Parse("2001-01-01"), result[0].LastCompleted);
-            Assert.AreEqual("Regular", result[0].Kind);
-            Assert.AreEqual("ABC123", result[0].Description);
-            Assert.AreEqual("Available", result[0].Status);
+            AssertValues(result[0],
+                number: "1001",
+                signedOut: null,
+                signedOutTo: "",
+                lastCompleted: DateTime.Parse("2001-01-01"),
+                lastCompletedBy: "Bruce Wayne",
+                kind: "Regular",
+                description: "ABC123",
+                status: "Available");
 
-            Assert.AreEqual("2002", result[1].Number);
-            Assert.AreEqual("Clark Kent", result[1].SignedOutTo);
-            Assert.AreEqual(DateTime.Parse("2002-02-02"), result[1].SignedOut);
-            Assert.AreEqual("", result[1].LastCompletedBy);
-            Assert.IsNull(result[1].LastCompleted);
-            Assert.AreEqual("Regular", result[1].Kind);
-            Assert.AreEqual("ABC123", result[1].Description);
-            Assert.AreEqual("Signed-out", result[1].Status);
+            AssertValues(result[1],
+                number: "2002",
+                signedOut: DateTime.Parse("2002-02-02"),
+                signedOutTo: "Clark Kent",
+                lastCompleted: null,
+                lastCompletedBy: string.Empty,
+                kind: "Regular",
+                description: "ABC123",
+                status: "Signed-out");
 
             Assert.AreEqual("3003", result[2].Number);
         }
@@ -64,6 +66,39 @@ namespace TerritoryTools.Alba.Controllers.Tests.AlbaBackupToS13
             Assert.AreEqual("Clark Kent", result[1].Publisher);
             Assert.AreEqual(DateTime.Parse("2002-02-02"), result[1].Date);
             Assert.AreEqual(AssignmentStatus.CheckedOut, result[1].Status);
+        }
+
+        public void AssertValues(AssignmentValues expected, AssignmentValues actual)
+        {
+            Assert.AreEqual(expected.Number, actual.Number);
+            Assert.AreEqual(expected.SignedOutTo, actual.SignedOutTo);
+            Assert.AreEqual(expected.SignedOut, actual.SignedOut);
+            Assert.AreEqual(expected.LastCompletedBy, actual.LastCompletedBy);
+            Assert.AreEqual(expected.LastCompleted, actual.LastCompleted);
+            Assert.AreEqual(expected.Kind, actual.Kind);
+            Assert.AreEqual(expected.Description, actual.Description);
+            Assert.AreEqual(expected.Status, actual.Status);
+        }
+
+        public void AssertValues(
+            AssignmentValues actual, 
+            string number = null,
+            DateTime? signedOut = null,
+            string signedOutTo = null,
+            DateTime? lastCompleted = null,
+            string lastCompletedBy = null,
+            string kind = null,
+            string description = null,
+            string status = null)
+        {
+            Assert.AreEqual(number, actual.Number);
+            Assert.AreEqual(signedOutTo ?? string.Empty, actual.SignedOutTo);
+            Assert.AreEqual(signedOut, actual.SignedOut);
+            Assert.AreEqual(lastCompletedBy ?? string.Empty, actual.LastCompletedBy);
+            Assert.AreEqual(lastCompleted, actual.LastCompleted);
+            Assert.AreEqual(kind, actual.Kind);
+            Assert.AreEqual(description, actual.Description);
+            Assert.AreEqual(status, actual.Status);
         }
     }
 }
