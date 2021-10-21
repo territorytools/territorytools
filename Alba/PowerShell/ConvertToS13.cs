6c713132f665949551929aba05763d9ea71c2f7e
Marc Durham <marcdurham@gmail.com>
2021-10-21 00:46:42 -0700

    Add PowerShell module for S13

---
+++ Alba/PowerShell/ConvertToS13.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Management.Automation;
+using TerritoryTools.Alba.Controllers.AlbaBackupToS13;
+
+namespace TerritoryTools.Alba.PowerShell
+{
+    [Cmdlet(VerbsData.ConvertTo, "S13")]
+    [OutputType(typeof(S13Entry))]
+    public class ConvertToS13 : PSCmdlet
+    {
+        [Parameter]
+        public string FolderPath { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                var s13entries = BackupFolder.LoadFolder(FolderPath);
+                WriteVerbose($"Entries Loaded: {s13entries.Count}");
+                foreach (var entry in s13entries)
+                {
+                    WriteObject(entry);
+                }
+            }
+            catch (Exception e)
+            {
+                WriteError(new ErrorRecord(e, "1", ErrorCategory.NotSpecified, null));
+            }
+
+            //base.EndProcessing();
+        }
+    }
+}
