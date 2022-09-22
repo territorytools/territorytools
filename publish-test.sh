fa805a9dbe31fba8042d6199b87e9d8bf1f16f78
Marc Durham <marcdurham@gmail.com>
2022-09-22 06:30:49 +0000

    Add script for deploy

--- publish-test.sh
+++ publish-test.sh
@@ -1,2 +1,2 @@
 git rev-parse HEAD > Web/MainSite/wwwroot/commit.txt
-docker-compose -f docker-compose.tt-web-test.yml build && docker-compose up -d
+docker-compose -f docker-compose.tt-web-test.yml build && docker-compose -f docker-compose.tt-web-test.yml up -d
