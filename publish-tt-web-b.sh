20b088e8839a46dbc2a77fa2fd3cbb2e1abc62c7
Marc Durham <marcdurham@gmail.com>
2022-09-30 06:02:18 +0000

    Add tags to publish command

--- publish-tt-web-b.sh
+++ publish-tt-web-b.sh
@@ -1,2 +1,5 @@
 git rev-parse HEAD > Web/MainSite/wwwroot/commit.txt
-docker-compose -f docker-compose.tt-web-b.yml build tt-web-b && docker-compose -f docker-compose.tt-web-b.yml up -d tt-web-b
+docker-compose -f docker-compose.tt-web-b.yml build tt-web-b \
+    && git tag tt-web-b/`date +%Y-%m-%d-%H%M%S`
+    && git push --tags
+    && docker-compose -f docker-compose.tt-web-b.yml up -d tt-web-b
