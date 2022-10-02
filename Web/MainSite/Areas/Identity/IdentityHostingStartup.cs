449f3783a4edd88bd1795540dd561fdf27cb45a5
Marc Durham <marcdurham@gmail.com>
2022-10-01 23:40:14 -0700

    Persist keys to /data/keys

--- Web/MainSite/Areas/Identity/IdentityHostingStartup.cs
+++ Web/MainSite/Areas/Identity/IdentityHostingStartup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.DependencyInjection;
 using TerritoryTools.Web.Data;
 using TerritoryTools.Entities;
 using TerritoryTools.Web.MainSite.Areas.UrlShortener.Models;
+using Microsoft.AspNetCore.DataProtection;
+using System.IO;
 
 [assembly: HostingStartup(typeof(TerritoryTools.Web.MainSite.Areas.Identity.IdentityHostingStartup))]
 namespace TerritoryTools.Web.MainSite.Areas.Identity
@@ -23,6 +25,12 @@ namespace TerritoryTools.Web.MainSite.Areas.Identity
 
                 services.AddDefaultIdentity<IdentityUser>()
                     .AddEntityFrameworkStores<MainDbContext>();
+
+                //https://learn.microsoft.com/en-us/aspnet/core/security/data-protection/configuration/overview?view=aspnetcore-3.1
+                //services.AddDataProtection()
+                //    .PersistKeysToDbContext<MainDbContext>();
+                services.AddDataProtection()
+                    .PersistKeysToFileSystem(new DirectoryInfo(@"/data/keys"));
 
                 //services.AddDefaultIdentity<IdentityUser>(
                 //    options => options.SignIn.RequireConfirmedAccount = true)
