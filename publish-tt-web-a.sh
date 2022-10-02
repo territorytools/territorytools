7d6da2282859752d882b8f0dd24ae689fb00d628
Marc Durham <marcdurham@gmail.com>
2022-10-02 06:21:24 +0000

    Add missing line continuation slashes

--- publish-tt-web-a.sh
+++ publish-tt-web-a.sh
@@ -1,5 +1,5 @@
 git rev-parse HEAD > Web/MainSite/wwwroot/commit.txt
 docker-compose -f docker-compose.tt-web-a.yml build tt-web-a \
-    && git tag tt-web-a/`date +%Y-%m-%d-%H%M%S`
-    && git push --tags
+    && git tag tt-web-a/`date +%Y-%m-%d-%H%M%S` \
+    && git push --tags \
     && docker-compose -f docker-compose.tt-web-a.yml up -d tt-web-a
