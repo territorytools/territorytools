d5c54defdcb8753508d8dd28a0ed8563648a4a82
Marc Durham <marcdurham@gmail.com>
2023-01-18 00:43:02 -0800

    Router works!

--- Web/RustYewSpa/src/main.rs
+++ Web/RustYewSpa/src/main.rs
@@ -24,7 +24,7 @@ impl Component for Model {
         true
     }
 
-    fn changed(&mut self, _ctx: &Context<Self>) -> bool {
+    fn changed(&mut self, _: &yew::Context<Self>, _: &<Self as yew::Component>::Properties) -> bool {
         false
     }
 
@@ -34,27 +34,60 @@ impl Component for Model {
         // });
 
         html! {
-            <Router render={switch} />
+            <p>{"Hi"}</p>
         }
     }
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    //yew::Renderer::<Main>();
+    yew::Renderer::<Main>::new().render();
+}
+
+#[function_component(Main)]
+fn app() -> Html {
+    html! {
+        // <TerritoryMap/>
+        <BrowserRouter>
+            <Switch<Route> render={switch} /> // <- must be child of <BrowserRouter>
+        </BrowserRouter>
+    }
 }
 
 #[derive(Clone, Routable, PartialEq)]
 enum Route {
     #[at("/")]
     Home,
+    #[at("/secure")]
+    Secure,
     #[at("/map")]
     Map,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+#[function_component(Secure)]
+fn secure() -> Html {
+    let navigator = use_navigator().unwrap();
+
+    let onclick = Callback::from(move |_| navigator.push(&Route::Home));
+    html! {
+        <div>
+            <h1>{ "Secure" }</h1>
+            <button {onclick}>{ "Go Home" }</button>
+        </div>
+    }
 }
 
 fn switch(route: Route) -> Html {
     match route {
         Route::Home => html! { <h1>{ "Home" }</h1> },
         Route::Map => html! { <TerritoryMap /> },
+        Route::Secure => html! {
+            <Secure />
+        },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
         //Route::Post { id } => html! {<p>{format!("You are looking at Post {}", id)}</p>},
         //Route::Misc { path } => html! {<p>{format!("Matched some other path: {}", path)}</p>},
     }
