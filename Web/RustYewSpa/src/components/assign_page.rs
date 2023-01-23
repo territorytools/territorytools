fdc14104186a2b34c0d280c828d648dc7a016fdc
Marc Durham <marcdurham@gmail.com>
2023-01-22 22:38:18 -0800

    Wow it compiles!

--- Web/RustYewSpa/src/components/assign_page.rs
+++ Web/RustYewSpa/src/components/assign_page.rs
@@ -13,6 +13,8 @@ const ASSIGN_METHOD: &str = "POST";
 // This is a good video: https://www.youtube.com/watch?v=2JNw-ftN6js
 // This is the GitHub repo: https://github.com/brooks-builds/full-stack-todo-rust-course/blob/1d8acb28951d0a019558b2afc43650ae5a0e718c/frontend/rust/yew/solution/src/api/patch_task.rs
 
+use std::clone;
+use std::ops::Deref;
 use crate::components::menu_bar::MenuBar;
 use crate::components::assign_form::*;
 use crate::models::territory_links::TerritoryLinkContract;
@@ -32,157 +34,148 @@ pub struct AssignPageProps {
 #[function_component(AssignPage)]
 pub fn assign_form(props: &AssignPageProps) -> Html {        
     //let link_contract: TerritoryLinkContract::default; 
-    // let territory_number: String = "".to_string();
-    // let assignee_email: String = "".to_string();
-    // let territory_uri: String = "".to_string();
-    // let link_contract = TerritoryLinkContract {
-    //     id: "".to_string(),
-    //     territory_uri: "".to_string(),
-    //     alba_mobile_territory_key: "".to_string(),
-    //     territory_number: "".to_string(),
-    //     assignee_email: "".to_string(),
-    //     assignee_phone: "".to_string(),
-    //     successful: false,
-    // };
+    let state = use_state(|| TerritoryLinkContract::default());
 
-    let onsubmit = {
+    let cloned_state = state.clone();
+    let onsubmit = Callback::from(move |assignment: TerritoryAssignment| {
         //let store_dispatch = store_dispatch.clone();
-        Callback::from(move |assignment: TerritoryAssignment| {
-            //let store_dispatch = store_dispatch.clone();
+        let cloned_state = cloned_state.clone();
+        spawn_local(async move {
+            //let result = api::login(user.username, user.password).await;
+            log!(format!("Territory Number: {}", assignment.territory_number));
+            log!(format!("Description: {}", assignment.description));
+            log!(format!("Assignee: {}", assignment.assignee));
+            let uri_string: String = format!("{path}?territoryNumber={number}&albaUserId={assignee}", 
+                path = DATA_API_PATH,
+                number = assignment.territory_number,
+                //descr = assignment.description,
+                assignee = assignment.assignee);
 
-            spawn_local(async move {
-                //let result = api::login(user.username, user.password).await;
-                log!(format!("Territory Number: {}", assignment.territory_number));
-                log!(format!("Description: {}", assignment.description));
-                log!(format!("Assignee: {}", assignment.assignee));
-                let uri_string: String = format!("{path}?territoryNumber={number}&albaUserId={assignee}", 
-                    path = DATA_API_PATH,
-                    number = assignment.territory_number,
-                    //descr = assignment.description,
-                    assignee = assignment.assignee);
+            //let description = assignment.description.clone();
+            let uri: &str = uri_string.as_str();
+            
+            let method: Method = match ASSIGN_METHOD {
+                "POST" => Method::POST,
+                "GET" => Method::GET,
+                &_ =>  Method::GET,
+            };
 
-                //let description = assignment.description.clone();
-                let uri: &str = uri_string.as_str();
+            let resp = Request::new(uri)
+                .method(method)
+                .header("Content-Type", "application/json")
+                //.body(format!("{{ 'description': '{description}' }}"))
+                .send()
+                .await
+                .unwrap();
+
+            let window = web_sys::window().expect("no global `window` exists");
+            let document = window.document().expect("should have a document on window");
+            let result_success = document
+                .get_element_by_id("result-success")
+                .expect("should have #to_be_hidden on the page")
+                .dyn_into::<web_sys::HtmlElement>()
+                .expect("#to_be_hidden should be an `HtmlElement`");
+            
+            let result_failure = document
+                .get_element_by_id("result-failure")
+                .expect("should have #to_be_hidden on the page")
+                .dyn_into::<web_sys::HtmlElement>()
+                .expect("#to_be_hidden should be an `HtmlElement`");
+
+            if resp.status() != 200 {
+                log!("Sorry the assignment failed.".to_string());
+                hide(result_success);
                 
-                let method: Method = match ASSIGN_METHOD {
-                    "POST" => Method::POST,
-                    "GET" => Method::GET,
-                    &_ =>  Method::GET,
-                };
-
-                let resp = Request::new(uri)
-                    .method(method)
-                    .header("Content-Type", "application/json")
-                    //.body(format!("{{ 'description': '{description}' }}"))
-                    .send()
+                let inner_text = format!("Failed to assign territory! Code: {}", resp.status());
+                
+                result_failure.set_inner_text(&inner_text);
+                show(result_failure);
+            } else {
+                log!("Yay the assignment succeeded!".to_string());
+                    
+                let link_contract: TerritoryLinkContract = resp
+                    .json()
                     .await
                     .unwrap();
 
-                let window = web_sys::window().expect("no global `window` exists");
-                let document = window.document().expect("should have a document on window");
-                let result_success = document
-                    .get_element_by_id("result-success")
-                    .expect("should have #to_be_hidden on the page")
-                    .dyn_into::<web_sys::HtmlElement>()
-                    .expect("#to_be_hidden should be an `HtmlElement`");
                 
-                let result_failure = document
-                    .get_element_by_id("result-failure")
-                    .expect("should have #to_be_hidden on the page")
+                // territory_number = link_contract.territory_number.clone();
+                // assignee_email = link_contract.assignee_email.clone();
+                // territory_uri = link_contract.territory_uri.clone();
+                hide(result_failure);
+
+                let result_link = document
+                    .get_element_by_id("result-link")
+                    .expect("should have #result-link on the page")
                     .dyn_into::<web_sys::HtmlElement>()
-                    .expect("#to_be_hidden should be an `HtmlElement`");
-
-                if resp.status() != 200 {
-                    log!("Sorry the assignment failed.".to_string());
-                    hide(result_success);
+                    .expect("#result-link should be an `HtmlElement`");
                     
-                    let inner_text = format!("Failed to assign territory! Code: {}", resp.status());
-                    
-                    result_failure.set_inner_text(&inner_text);
-                    show(result_failure);
-                } else {
-                    log!("Yay the assignment succeeded!".to_string());
-                       
-                    let link_contract: TerritoryLinkContract = resp
-                        .json()
-                        .await
-                        .unwrap();
-                    
-                    // territory_number = link_contract.territory_number.clone();
-                    // assignee_email = link_contract.assignee_email.clone();
-                    // territory_uri = link_contract.territory_uri.clone();
-                    hide(result_failure);
-
-                    let result_link = document
-                        .get_element_by_id("result-link")
-                        .expect("should have #result-link on the page")
-                        .dyn_into::<web_sys::HtmlElement>()
-                        .expect("#result-link should be an `HtmlElement`");
-                        
-                    result_link.set_inner_text(&link_contract.territory_uri);
-                    result_link
-                        .set_attribute("href", &link_contract.territory_uri)
-                        .expect("Attribute href should have been set");
-                    
-                    let sms_link_href = format!(
-                        "sms://{assignee_phone}?body=Territory%20{territory_number}%20{territory_uri}",
-                        assignee_phone = link_contract.assignee_phone,
-                        territory_number = link_contract.territory_number,
-                        territory_uri = link_contract.territory_uri,
-                    );
-                    
-                    let sms_link = document
-                        .get_element_by_id("sms-link")
-                        .expect("should have #sms-link on the page")
-                        .dyn_into::<web_sys::HtmlElement>()
-                        .expect("#sms-link should be an `HtmlElement`");
-                    
-                    sms_link
-                        .set_attribute("href", &sms_link_href)
-                        .expect("Attribute href should have been set");
-                    
-                    let sms_number = document
-                        .get_element_by_id("sms-number")
-                        .expect("should have #sms-number on the page")
-                        .dyn_into::<web_sys::HtmlElement>()
-                        .expect("#sms-link should be an `HtmlElement`");
-
-                    sms_number
-                        .set_attribute("value", &link_contract.assignee_phone)
-                        .expect("Value should have been set");
-                     
-                    // let email_link_href = format!(
-                    //     "mailto://{assignee_email}?body=Territory%20{territory_number}%20{territory_uri}",
-                    //     assignee_email = link_contract.assignee_email,
-                    //     territory_number = link_contract.territory_number,
-                    //     territory_uri = link_contract.territory_uri,
-                    // );
-
-                    // let email_link = document
-                    //     .get_element_by_id("email-link")
-                    //     .expect("should have #email-link on the page")
-                    //     .dyn_into::<web_sys::HtmlElement>()
-                    //     .expect("#email-link should be an `HtmlElement`");
+                result_link.set_inner_text(&link_contract.territory_uri);
+                result_link
+                    .set_attribute("href", &link_contract.territory_uri)
+                    .expect("Attribute href should have been set");
                 
-                    // email_link
-                    //     .set_attribute("href", &email_link_href)
-                    //     .expect("Attribute href should have been set");
-                    
-                    // let email_address = document
-                    //     .get_element_by_id("email-address")
-                    //     .expect("should have #email-address on the page")
-                    //     .dyn_into::<web_sys::HtmlElement>()
-                    //     .expect("#email-link should be an `HtmlElement`");
+                let sms_link_href = format!(
+                    "sms://{assignee_phone}?body=Territory%20{territory_number}%20{territory_uri}",
+                    assignee_phone = link_contract.assignee_phone,
+                    territory_number = link_contract.territory_number,
+                    territory_uri = link_contract.territory_uri,
+                );
+                
+                let sms_link = document
+                    .get_element_by_id("sms-link")
+                    .expect("should have #sms-link on the page")
+                    .dyn_into::<web_sys::HtmlElement>()
+                    .expect("#sms-link should be an `HtmlElement`");
+                
+                sms_link
+                    .set_attribute("href", &sms_link_href)
+                    .expect("Attribute href should have been set");
+                
+                let sms_number = document
+                    .get_element_by_id("sms-number")
+                    .expect("should have #sms-number on the page")
+                    .dyn_into::<web_sys::HtmlElement>()
+                    .expect("#sms-link should be an `HtmlElement`");
 
-                    // email_address
-                    //     .set_attribute("value", &link_contract.assignee_email)
-                    //     .expect("Value should have been set");
-                    
-                    show(result_success);
-                }
-            });
-        })
-    };
+                sms_number
+                    .set_attribute("value", &link_contract.assignee_phone)
+                    .expect("Value should have been set");
+                
+                //let contract = cloned_state.deref().clone();    
+                cloned_state.set(link_contract);
+                
+                // let email_link_href = format!(
+                //     "mailto://{assignee_email}?body=Territory%20{territory_number}%20{territory_uri}",
+                //     assignee_email = link_contract.assignee_email,
+                //     territory_number = link_contract.territory_number,
+                //     territory_uri = link_contract.territory_uri,
+                // );
+
+                // let email_link = document
+                //     .get_element_by_id("email-link")
+                //     .expect("should have #email-link on the page")
+                //     .dyn_into::<web_sys::HtmlElement>()
+                //     .expect("#email-link should be an `HtmlElement`");
+            
+                // email_link
+                //     .set_attribute("href", &email_link_href)
+                //     .expect("Attribute href should have been set");
+                
+                // let email_address = document
+                //     .get_element_by_id("email-address")
+                //     .expect("should have #email-address on the page")
+                //     .dyn_into::<web_sys::HtmlElement>()
+                //     .expect("#email-link should be an `HtmlElement`");
+
+                // email_address
+                //     .set_attribute("value", &link_contract.assignee_email)
+                //     .expect("Value should have been set");
+                
+                show(result_success);
+            }
+        });
+    });
 
     html! {
         <>
@@ -209,7 +202,7 @@ pub fn assign_form(props: &AssignPageProps) -> Html {
                     <EmailSection 
                         territory_number={"territory_number"}
                         assignee_email={"assignee_email"}
-                        territory_uri={"territory_uri"}
+                        territory_uri={state.territory_uri.clone()}
                     />
                     // <div id={"email-section"} class={"form-group"}>
                     // <label for={"email-address"}>{"Send as email message:"}</label>
