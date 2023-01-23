ec21abf0c8a11d192ab81d9c23fa0abc053c28c1
Marc Durham <marcdurham@gmail.com>
2023-01-22 23:29:27 -0800

    Clean up and refactor

--- Web/RustYewSpa/src/components/assign_page.rs
+++ Web/RustYewSpa/src/components/assign_page.rs
@@ -13,14 +13,13 @@ const ASSIGN_METHOD: &str = "POST";
 // This is a good video: https://www.youtube.com/watch?v=2JNw-ftN6js
 // This is the GitHub repo: https://github.com/brooks-builds/full-stack-todo-rust-course/blob/1d8acb28951d0a019558b2afc43650ae5a0e718c/frontend/rust/yew/solution/src/api/patch_task.rs
 
-use crate::components::menu_bar::MenuBar;
 use crate::components::assign_form::*;
 use crate::components::email_section::EmailSection;
+use crate::components::menu_bar::MenuBar;
 use crate::components::sms_section::SmsSection;
 use crate::models::territory_links::TerritoryLinkContract;
 use reqwasm::http::{Request, Method};
 use wasm_bindgen_futures::spawn_local;
-use wasm_bindgen::JsCast;
 use yew::prelude::*;
 
 #[derive(Properties, PartialEq, Clone, Default)]
@@ -49,10 +48,8 @@ pub fn assign_form(props: &AssignPageProps) -> Html {
             let uri_string: String = format!("{path}?territoryNumber={number}&albaUserId={assignee}", 
                 path = DATA_API_PATH,
                 number = assignment.territory_number,
-                //descr = assignment.description,
                 assignee = assignment.assignee);
 
-            //let description = assignment.description.clone();
             let uri: &str = uri_string.as_str();
             
             let method: Method = match ASSIGN_METHOD {
@@ -69,55 +66,20 @@ pub fn assign_form(props: &AssignPageProps) -> Html {
                 .await
                 .unwrap();
 
-            // let window = web_sys::window().expect("no global `window` exists");
-            // let document = window.document().expect("should have a document on window");
-            // let result_success = document
-            //     .get_element_by_id("result-success")
-            //     .expect("should have #to_be_hidden on the page")
-            //     .dyn_into::<web_sys::HtmlElement>()
-            //     .expect("#to_be_hidden should be an `HtmlElement`");
-            
-            // let result_failure = document
-            //     .get_element_by_id("result-failure")
-            //     .expect("should have #to_be_hidden on the page")
-            //     .dyn_into::<web_sys::HtmlElement>()
-            //     .expect("#to_be_hidden should be an `HtmlElement`");
-
-            if resp.status() != 200 {
-                //hide(result_success);
-                
-                //let inner_text = format!("Failed to assign territory! Code: {}", resp.status());
-                
-                //result_failure.set_inner_text(&inner_text);
-
-                let result = AssignmentResult {
-                    success: false,
-                    link_contract: TerritoryLinkContract::default(),
-                    status: resp.status(),
-                    completed: true,
-                };
-                
-                cloned_state.set(result);
-
-                //show(result_failure);
+            let link_contract: TerritoryLinkContract = if resp.status() == 200 {
+                resp.json().await.unwrap()
             } else {
-                //hide(result_failure);
+                TerritoryLinkContract::default()
+            };
+            
+            let result = AssignmentResult {
+                success: (resp.status() == 200),
+                link_contract: link_contract,
+                status: resp.status(),
+                completed: true,
+            };
 
-                let link_contract: TerritoryLinkContract = resp
-                    .json()
-                    .await
-                    .unwrap();
-                
-                let result = AssignmentResult {
-                    success: true,
-                    link_contract: link_contract,
-                    status: resp.status(),
-                    completed: true,
-                };
-
-                cloned_state.set(result);
-                //show(result_success);
-            }
+            cloned_state.set(result);
         });
     });
 
@@ -159,19 +121,3 @@ pub fn assign_form(props: &AssignPageProps) -> Html {
         </>
     }
 }
-
-
-fn show(element: web_sys::HtmlElement) {
-    element
-        .style()
-        .set_property("display", "block")
-        .expect("'display' should have been set to 'block'")
-}
-
-fn hide(element: web_sys::HtmlElement) {
-    element
-        .style()
-        .set_property("display", "none")
-        .expect("'display' should have been set to 'none'")
-}
-
