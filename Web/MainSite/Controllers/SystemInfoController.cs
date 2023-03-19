c83bf3ae569f93f9f7cfc712a7865601222d59cc
Marc Durham <marcdurham@gmail.com>
2023-03-19 00:19:04 -0700

    Remove secrets from SystemInfo

--- Web/MainSite/Controllers/SystemInfoController.cs
+++ Web/MainSite/Controllers/SystemInfoController.cs
@@ -1,23 +1,14 @@
 ﻿
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Primitives;
 using System;
 using TerritoryTools.Web.MainSite.Services;
 
 namespace TerritoryTools.Web.MainSite.Controllers
 {
-
-    public class SystemInfo
-    {
-        public string? ApiDatabaseName { get; set; }
-        public string? ApiGitCommit { get; set; }
-        public DatabaseInfo DatabaseInfo { get; set; } = new();
-        public string? MachineName { get; set; }
-        public WebUIOptions WebUIOptions { get; set; } = new();
-    }
-
+    [Authorize]
     [ApiController]
     [Route("system-info")]
     public partial class SystemInfoController : Controller
@@ -27,6 +18,7 @@ namespace TerritoryTools.Web.MainSite.Controllers
         private readonly ILogger<SystemInfoController> _logger;
 
         private SystemInfo _systemInfo = new();
+        private WebUIOptions _webUIOptions = new();
 
         public SystemInfoController(
             IDatabaseInfoService databaseInfoService,
@@ -37,7 +29,7 @@ namespace TerritoryTools.Web.MainSite.Controllers
             _configuration = configuration;
             _logger = logger;
 
-            _configuration.Bind(_systemInfo.WebUIOptions);
+            _configuration.Bind(_webUIOptions);
         }
 
         [HttpGet]
@@ -46,6 +38,28 @@ namespace TerritoryTools.Web.MainSite.Controllers
             _systemInfo.DatabaseInfo = _databaseInfoService.Info();
             _systemInfo.MachineName = Environment.MachineName;
             _systemInfo.ApiGitCommit = _configuration.GetValue<string>("GitCommit");
+
+            // From WebUIOptions except secrets
+            _systemInfo.AlbaHost = _webUIOptions.AlbaHost;
+            _systemInfo.AlbaUserManagementHtmlPath = _webUIOptions.AlbaUserManagementHtmlPath;
+            _systemInfo.AlbaUsersHtmlPath = _webUIOptions.AlbaUsersHtmlPath;
+            _systemInfo.AlbaAssignmentsHtmlPath = _webUIOptions.AlbaAssignmentsHtmlPath;
+            _systemInfo.AzureAppId = _webUIOptions.AzureAppId;
+            _systemInfo.UrlShortenerDomain = _webUIOptions.UrlShortenerDomain;
+            _systemInfo.CompletionMapUrl = _webUIOptions.CompletionMapUrl;
+            _systemInfo.Users = _webUIOptions.Users;
+            _systemInfo.AdminUsers = _webUIOptions.AdminUsers;
+            _systemInfo.DefaultPhoneTerritorySourceDocumentId = _webUIOptions.DefaultPhoneTerritorySourceDocumentId;
+            _systemInfo.DefaultPhoneTerritorySourceSheetName = _webUIOptions.DefaultPhoneTerritorySourceSheetName;
+            _systemInfo.SharedPhoneTerritoryEmailAddress = _webUIOptions.SharedPhoneTerritoryEmailAddress;
+            _systemInfo.SharedPhoneTerritoryFullName = _webUIOptions.SharedPhoneTerritoryFullName;
+            _systemInfo.SmsApiUserName = _webUIOptions.SmsApiUserName;
+            _systemInfo.SmsFromPhoneNumber = _webUIOptions.SmsFromPhoneNumber;
+            _systemInfo.SmsMessageLogDocumentId = _webUIOptions.SmsMessageLogDocumentId;
+            _systemInfo.SmsMessageLogSheetName = _webUIOptions.SmsMessageLogSheetName;
+            _systemInfo.SmsAdminRecipient = _webUIOptions.SmsAdminRecipient;
+            _systemInfo.TerritoryApiBaseUrl = _webUIOptions.TerritoryApiBaseUrl;
+            _systemInfo.Features = _webUIOptions.Features;
 
             return Ok(_systemInfo);
         }
---
+++ Web/MainSite/Models/SystemInfo.cs
@@ -0,0 +1,32 @@
+﻿using TerritoryTools.Web.MainSite.Services;
+
+namespace TerritoryTools.Web.MainSite.Controllers
+{
+    public class SystemInfo
+    {
+        public string? ApiDatabaseName { get; set; }
+        public string? ApiGitCommit { get; set; }
+        public DatabaseInfo DatabaseInfo { get; set; } = new();
+        public string? MachineName { get; set; }
+        public string AlbaHost { get; set; }
+        public string AlbaUserManagementHtmlPath { get; set; }
+        public string AlbaUsersHtmlPath { get; set; }
+        public string AlbaAssignmentsHtmlPath { get; set; }
+        public string AzureAppId { get; set; }
+        public string UrlShortenerDomain { get; set; }
+        public string CompletionMapUrl { get; set; }
+        public string Users { get; set; }
+        public string AdminUsers { get; set; }
+        public string DefaultPhoneTerritorySourceDocumentId { get; set; }
+        public string DefaultPhoneTerritorySourceSheetName { get; set; }
+        public string SharedPhoneTerritoryEmailAddress { get; set; }
+        public string SharedPhoneTerritoryFullName { get; set; }
+        public string SmsApiUserName { get; set; }
+        public string SmsFromPhoneNumber { get; set; }
+        public string SmsMessageLogDocumentId { get; set; }
+        public string SmsMessageLogSheetName { get; set; }
+        public string SmsAdminRecipient { get; set; }
+        public string TerritoryApiBaseUrl { get; set; }
+        public Features Features { get; set; } = new Features();
+    }
+}
