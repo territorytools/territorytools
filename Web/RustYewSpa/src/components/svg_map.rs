45044f715248e12abf8dd29a61da347b6ee61569
Marc Durham <marcdurham@gmail.com>
2023-05-21 21:55:26 -0700

    Put transform origin where the cursor is

--- Web/RustYewSpa/src/components/svg_map.rs
+++ Web/RustYewSpa/src/components/svg_map.rs
@@ -1,6 +1,6 @@
 use gloo_console::log;
-use yew::prelude::*;
 use web_sys::HtmlElement;
+use yew::prelude::*;
 
 #[derive(Properties, PartialEq, Clone, Default)]
 pub struct SvgMapModel {
@@ -23,24 +23,24 @@ pub struct SvgMapModel {
 }
 
 #[function_component(SvgMap)]
-pub fn svg_map() -> Html { 
-    let state: yew::UseStateHandle<SvgMapModel> = use_state(|| SvgMapModel { 
-        mx: 0.0, 
-        my: 0.0, 
+pub fn svg_map() -> Html {
+    let state: yew::UseStateHandle<SvgMapModel> = use_state(|| SvgMapModel {
+        mx: 0.0,
+        my: 0.0,
         mouse_down: false,
-        pane_x: 0.0, 
-        pane_y: 0.0, 
-        pane_start_x: 0.0, 
-        pane_start_y: 0.0, 
-        start_pan_x: 0.0, 
-        start_pan_y: 0.0, 
-        x_delta: 0.0, 
-        y_delta: 0.0, 
+        pane_x: 0.0,
+        pane_y: 0.0,
+        pane_start_x: 0.0,
+        pane_start_y: 0.0,
+        start_pan_x: 0.0,
+        start_pan_y: 0.0,
+        x_delta: 0.0,
+        y_delta: 0.0,
         wheel_delta: 0.0,
         pinch_width_start: 0.0,
         pinch_width: 0.0,
         pinch_ratio: 1.0,
-        zoom: 1.0, 
+        zoom: 1.0,
     });
     let cloned_state = state.clone();
     let onmousemove = Callback::from(move |e: MouseEvent| {
@@ -53,12 +53,12 @@ pub fn svg_map() -> Html {
             if cloned_state.mouse_down {
                 let x_delta = cloned_state.mx - x;
                 let y_delta = cloned_state.my - y;
-                cloned_state.set(SvgMapModel { 
-                    mx: x, 
+                cloned_state.set(SvgMapModel {
+                    mx: x,
                     my: y,
                     mouse_down: cloned_state.mouse_down,
                     pane_x: cloned_state.pane_x - x_delta,
-                    pane_y: cloned_state.pane_y - y_delta,                    
+                    pane_y: cloned_state.pane_y - y_delta,
                     pane_start_x: cloned_state.pane_start_x,
                     pane_start_y: cloned_state.pane_start_y,
                     start_pan_x: cloned_state.start_pan_x,
@@ -72,8 +72,8 @@ pub fn svg_map() -> Html {
                     zoom: cloned_state.zoom,
                 });
             } else {
-                cloned_state.set(SvgMapModel { 
-                    mx: x, 
+                cloned_state.set(SvgMapModel {
+                    mx: x,
                     my: y,
                     mouse_down: cloned_state.mouse_down,
                     pane_x: cloned_state.pane_x,
@@ -107,15 +107,21 @@ pub fn svg_map() -> Html {
                 let touch_1 = e.touches().item(1).expect("Second touch object");
                 let w = (touch_0.client_x() - touch_1.client_x()).abs() as f64;
                 let h = (touch_0.client_y() - touch_1.client_y()).abs() as f64;
-                if w >= h { w } else { h }
-            } else { 0.0 };
+                if w >= h {
+                    w
+                } else {
+                    h
+                }
+            } else {
+                0.0
+            };
 
-            cloned_state.set(SvgMapModel { 
-                mx: x, 
+            cloned_state.set(SvgMapModel {
+                mx: x,
                 my: y,
                 mouse_down: cloned_state.mouse_down,
                 pane_x: cloned_state.pane_x,
-                pane_y: cloned_state.pane_y,                    
+                pane_y: cloned_state.pane_y,
                 pane_start_x: cloned_state.pane_start_x,
                 pane_start_y: cloned_state.pane_start_y,
                 start_pan_x: cloned_state.start_pan_x,
@@ -144,32 +150,47 @@ pub fn svg_map() -> Html {
                 let touch_1 = e.touches().item(1).expect("Second touch object");
                 let w = (touch_0.client_x() - touch_1.client_x()).abs() as f64;
                 let h = (touch_0.client_y() - touch_1.client_y()).abs() as f64;
-                if w >= h { w } else { h }
-            } else { 0.0 };
+                if w >= h {
+                    w
+                } else {
+                    h
+                }
+            } else {
+                0.0
+            };
 
             let pinch_ratio: f64 = if cloned_state.pinch_width_start > 1.0 && wide > 1.0 {
-                 let ratio = ((wide as f64)/cloned_state.pinch_width_start).abs();
-                 if ratio >= 2.0 
-                 { 
-                    2.0 
-                } else { 
-                    if ratio < 0.5 { 0.5 } else { ratio }
+                let ratio = ((wide as f64) / cloned_state.pinch_width_start).abs();
+                if ratio >= 2.0 {
+                    2.0
+                } else {
+                    if ratio < 0.5 {
+                        0.5
+                    } else {
+                        ratio
+                    }
                 }
-            } else { 1.0 };
+            } else {
+                1.0
+            };
 
             let mut zoom: f64 = cloned_state.zoom * pinch_ratio;
-            if zoom > 12.0 { zoom = 12.0 };
-            if zoom < 0.1 { zoom = 0.1 };
+            if zoom > 12.0 {
+                zoom = 12.0
+            };
+            if zoom < 0.1 {
+                zoom = 0.1
+            };
 
             log!(format!("Left? : {} ; Top? : {}", x, y));
             let x_delta = cloned_state.mx - x;
             let y_delta = cloned_state.my - y;
-            cloned_state.set(SvgMapModel { 
-                mx: x, 
+            cloned_state.set(SvgMapModel {
+                mx: x,
                 my: y,
                 mouse_down: cloned_state.mouse_down,
                 pane_x: cloned_state.pane_x - x_delta,
-                pane_y: cloned_state.pane_y - y_delta,                    
+                pane_y: cloned_state.pane_y - y_delta,
                 pane_start_x: cloned_state.pane_start_x,
                 pane_start_y: cloned_state.pane_start_y,
                 start_pan_x: cloned_state.start_pan_x,
@@ -192,14 +213,14 @@ pub fn svg_map() -> Html {
             let rect = target.get_bounding_client_rect();
             let x = (e.client_x() as f64) - rect.left();
             let y = (e.client_y() as f64) - rect.top();
-            cloned_state.set(SvgMapModel { 
+            cloned_state.set(SvgMapModel {
                 mx: x,
                 my: y,
                 mouse_down: true,
                 pane_x: cloned_state.pane_x,
                 pane_y: cloned_state.pane_y,
                 pane_start_x: cloned_state.pane_start_x,
-                pane_start_y: cloned_state.pane_start_y,                
+                pane_start_y: cloned_state.pane_start_y,
                 start_pan_x: cloned_state.mx,
                 start_pan_y: cloned_state.my,
                 x_delta: 0.0,
@@ -217,13 +238,12 @@ pub fn svg_map() -> Html {
     let onmouseup = Callback::from(move |e: MouseEvent| {
         let cloned_state = cloned_state.clone();
         if let Some(_target) = e.target_dyn_into::<HtmlElement>() {
-
             // let x_delta = cloned_state.start_pan_x - cloned_state.mx;
             // let y_delta = cloned_state.start_pan_y - cloned_state.my;
 
-           cloned_state.set(SvgMapModel { 
+            cloned_state.set(SvgMapModel {
                 mx: cloned_state.mx,
-                my: cloned_state.my, 
+                my: cloned_state.my,
                 mouse_down: false,
                 pane_x: cloned_state.pane_x, // - x_delta,
                 pane_y: cloned_state.pane_y, // - y_delta,
@@ -246,13 +266,12 @@ pub fn svg_map() -> Html {
     let onmouseleave = Callback::from(move |e: MouseEvent| {
         let cloned_state = cloned_state.clone();
         if let Some(_target) = e.target_dyn_into::<HtmlElement>() {
-
             // let x_delta = cloned_state.start_pan_x - cloned_state.mx;
             // let y_delta = cloned_state.start_pan_y - cloned_state.my;
 
-           cloned_state.set(SvgMapModel { 
+            cloned_state.set(SvgMapModel {
                 mx: cloned_state.mx,
-                my: cloned_state.my, 
+                my: cloned_state.my,
                 mouse_down: false,
                 pane_x: cloned_state.pane_x, // - x_delta,
                 pane_y: cloned_state.pane_y, // - y_delta,
@@ -275,16 +294,19 @@ pub fn svg_map() -> Html {
     let onwheel = Callback::from(move |e: WheelEvent| {
         let cloned_state = cloned_state.clone();
         if let Some(_target) = e.target_dyn_into::<HtmlElement>() {
-
             let wheel_y = e.delta_y() as f64;
             let mut zoom = cloned_state.zoom - (wheel_y / 300.0);
 
-            if zoom > 12.0 { zoom = 12.0 };
-            if zoom < 0.1 { zoom = 0.1 };
+            if zoom > 12.0 {
+                zoom = 12.0
+            };
+            if zoom < 0.1 {
+                zoom = 0.1
+            };
 
-            cloned_state.set(SvgMapModel { 
+            cloned_state.set(SvgMapModel {
                 mx: cloned_state.mx,
-                my: cloned_state.my, 
+                my: cloned_state.my,
                 mouse_down: false,
                 pane_x: cloned_state.pane_x, // - x_delta,
                 pane_y: cloned_state.pane_y, // - y_delta,
@@ -304,19 +326,21 @@ pub fn svg_map() -> Html {
     });
 
     let cloned_state = state.clone();
-    //{"width:auto;background-color:red;transform: translate3d(" + cloned_state.mx + "px, " +  cloned_state.my + "px, 0px);"} 
+    //{"width:auto;background-color:red;transform: translate3d(" + cloned_state.mx + "px, " +  cloned_state.my + "px, 0px);"}
 
     let _mouse_x = cloned_state.mx;
     let _mouse_y = cloned_state.my;
     let pane_x = cloned_state.pane_x;
     let pane_y = cloned_state.pane_y;
+    let pane_cursor_x = cloned_state.mx - cloned_state.pane_x;
+    let pane_cursor_y = cloned_state.my - cloned_state.pane_y;
 
-    html!{
+    html! {
         <div style="height:100%;">
             <div style="height:100px;">
                 <span>{" Mouse: "}{cloned_state.mx}{", "}{cloned_state.my}{" Mouse Down:"}{cloned_state.mouse_down}</span><br/>
-                
-                <span>{" Pane: "}{cloned_state.pane_x}{", "}{cloned_state.pane_y}</span>
+
+                <span>{" Pane: "}{cloned_state.pane_x}{", "}{cloned_state.pane_y}{" Cursor: "}{pane_cursor_x}{", "}{pane_cursor_y}</span>
                 <span>{" Zoom: "}{cloned_state.zoom}</span><br/>
 
                 <span>{" Pinch: start: "}{cloned_state.pinch_width_start}
@@ -326,57 +350,60 @@ pub fn svg_map() -> Html {
                 <span>{" Delta: "}{cloned_state.x_delta}{", "}{cloned_state.y_delta}{" Wheel: "}{cloned_state.wheel_delta}</span>
             </div>
 
-        <div {onmousemove} {onmousedown} {onmouseup} {onmouseleave} {onwheel} {ontouchmove} {ontouchstart}
-            style="touch-action: pinch-zoom;width:100%;height:calc(100% - 100px);background-color:gray;overflow:hidden;">
-        <div style={format!(" pointer-events: none;width:auto;background-color:red;transform: translate3d({}px, {}px, 0px) scale({}, {});", pane_x, pane_y, cloned_state.zoom, cloned_state.zoom)} >
-        
-        //<svg width="149" height="147" viewBox="0 0 149 147" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <svg width="700" height="1024" viewBox="0 0 700 512" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <image x="-56" y="-156"
-                width="256" height="256"
-                //xlink:href="data:image/png;base64,IMAGE_DATA"
-                href="https://c.tile.openstreetmap.org/11/328/710.png"
-                />
-            <image x="200" y="100"
-                width="256" height="256"
-                //xlink:href="data:image/png;base64,IMAGE_DATA"
-                href="https://c.tile.openstreetmap.org/11/329/711.png"
-                />
-            <image x="200" y="356"
-                width="256" height="256"
-                //xlink:href="data:image/png;base64,IMAGE_DATA"
-                href="https://c.tile.openstreetmap.org/11/329/712.png"
-                />
-            <image x="200" y="612"
-                width="256" height="256"
-                //xlink:href="data:image/png;base64,IMAGE_DATA"
-                href="https://c.tile.openstreetmap.org/11/329/713.png"
-                />
-            <image x="456" y="100"
-                width="256" height="256"
-                //xlink:href="data:image/png;base64,IMAGE_DATA"
-                href="https://c.tile.openstreetmap.org/11/330/711.png"
-                />
-            <image x="456" y="356"
-                width="256" height="256"
-                //xlink:href="data:image/png;base64,IMAGE_DATA"
-                href="https://c.tile.openstreetmap.org/11/330/712.png"
-                />
-            <image x="456" y="612"
-                width="256" height="256"
-                //xlink:href="data:image/png;base64,IMAGE_DATA"
-                href="https://c.tile.openstreetmap.org/11/330/713.png"
-                />
-            <path d="M60.5776 13.8268L51.8673 42.6431L77.7475 37.331L60.5776 13.8268Z" fill="#DEB819"/>
-            <path d="M108.361 94.9937L138.708 90.686L115.342 69.8642" stroke="black" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-            <g>
-                <circle cx="75.3326" cy="73.4918" r="55" fill="#FDD630"/>
-                <circle cx="75.3326" cy="73.4918" r="52.5" stroke="black" stroke-width="5"/>
-            </g>
-            <circle cx="71" cy="99" r="5" fill="white" fill-opacity="0.75" stroke="black" stroke-width="3"/>
-        </svg>
-        </div>
-        </div>
+            <div {onmousemove} {onmousedown} {onmouseup} {onmouseleave} {onwheel} {ontouchmove} {ontouchstart}
+                style="touch-action: none;width:100%;height:calc(100% - 100px);background-color:gray;overflow:hidden;">
+                <div style={format!(
+                    "transform-origin: {}px {}px;pointer-events: none;width:500px;height:500px;background-color:red;transform: translate3d({}px, {}px, 0px) scale({}, {});",
+                    pane_cursor_x, pane_cursor_y, pane_x, pane_y, cloned_state.zoom, cloned_state.zoom)} >
+
+                //<svg width="149" height="147" viewBox="0 0 149 147" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="700" height="1024" viewBox="0 0 700 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <image x="-56" y="-156"
+                        width="256" height="256"
+                        //xlink:href="data:image/png;base64,IMAGE_DATA"
+                        href="https://c.tile.openstreetmap.org/11/328/710.png"
+                        />
+                    <image x="200" y="100"
+                        width="256" height="256"
+                        //xlink:href="data:image/png;base64,IMAGE_DATA"
+                        href="https://c.tile.openstreetmap.org/11/329/711.png"
+                        />
+                    <image x="200" y="356"
+                        width="256" height="256"
+                        //xlink:href="data:image/png;base64,IMAGE_DATA"
+                        href="https://c.tile.openstreetmap.org/11/329/712.png"
+                        />
+                    <image x="200" y="612"
+                        width="256" height="256"
+                        //xlink:href="data:image/png;base64,IMAGE_DATA"
+                        href="https://c.tile.openstreetmap.org/11/329/713.png"
+                        />
+                    <image x="456" y="100"
+                        width="256" height="256"
+                        //xlink:href="data:image/png;base64,IMAGE_DATA"
+                        href="https://c.tile.openstreetmap.org/11/330/711.png"
+                        />
+                    <image x="456" y="356"
+                        width="256" height="256"
+                        //xlink:href="data:image/png;base64,IMAGE_DATA"
+                        href="https://c.tile.openstreetmap.org/11/330/712.png"
+                        />
+                    <image x="456" y="612"
+                        width="256" height="256"
+                        //xlink:href="data:image/png;base64,IMAGE_DATA"
+                        href="https://c.tile.openstreetmap.org/11/330/713.png"
+                        />
+                    <path d="M60.5776 13.8268L51.8673 42.6431L77.7475 37.331L60.5776 13.8268Z" fill="#DEB819"/>
+                    <path d="M108.361 94.9937L138.708 90.686L115.342 69.8642" stroke="black" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                    <g>
+                        <circle cx="75.3326" cy="73.4918" r="55" fill="#FDD630"/>
+                        <circle cx="75.3326" cy="73.4918" r="52.5" stroke="black" stroke-width="5"/>
+                    </g>
+                    <circle cx="71" cy="99" r="5" fill="white" fill-opacity="0.75" stroke="black" stroke-width="3"/>
+                    <circle cx="0" cy="0" r="8" fill="yellow" fill-opacity="0.75" stroke="black" stroke-width="3"/>
+                </svg>
+                </div>
+            </div>
         </div>
     }
-} 
+}
