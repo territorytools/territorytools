01a952141648bdb49d82dec7f29d492e525f0b5b
Marc Durham <marcdurham@gmail.com>
2023-09-10 21:42:27 -0700

    Fix color on button, add CustomButton for menus

---
+++ Web/MainSite/Models/CustomButton.cs
@@ -0,0 +1,9 @@
+﻿namespace TerritoryTools.Web.MainSite.Models
+{
+    public class CustomButton
+    {
+        public string Page { get; set; }
+        public string Text { get; set; }
+        public string Uri { get; set; }
+    }
+}
