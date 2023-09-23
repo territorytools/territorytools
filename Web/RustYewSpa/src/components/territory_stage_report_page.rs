cd7daa77c397847075614e895ce35a6e2befd261
Marc Durham <marcdurham@gmail.com>
2023-09-23 00:30:24 -0700

    Clean up report page

--- Web/RustYewSpa/src/components/territory_stage_report_page.rs
+++ Web/RustYewSpa/src/components/territory_stage_report_page.rs
@@ -6,17 +6,12 @@ const DATA_API_PATH: &str = "/api/territory-stage-reports/monthly-completion";
 
 use crate::{components::menu_bar_v2::MenuBarV2, models::territory_reports::MonthCompletionSummary};
 use crate::components::menu_bar::MapPageLink;
-use crate::models::territories::TerritorySummary;
-use crate::Route;
 
 use gloo_console::log;
 use reqwasm::http::Request;
 use yew::prelude::*;
 use yew_router::prelude::LocationHandle;
 use yew_router::scope_ext::RouterScopeExt;
-use wasm_bindgen::JsCast;
-use web_sys::HtmlInputElement;
-use serde::{Serialize, Deserialize};
 
 pub enum Msg {
     Load(TerritoryStageReportResult),
@@ -43,7 +38,7 @@ impl Component for TerritoryStageReportPage {
             )
             .unwrap();
 
-        return Self {
+        Self {
             _listener: listener,
             months: vec![],
             result: TerritoryStageReportResult::default(),
@@ -65,10 +60,10 @@ impl Component for TerritoryStageReportPage {
         }
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
+    fn view(&self, _ctx: &Context<Self>) -> Html {
         //set_document_title("Territory Search");
        
-        let count = self.months.len();
+        let _count = self.months.len();
         html!{
             <>
                 <MenuBarV2>
@@ -84,10 +79,10 @@ impl Component for TerritoryStageReportPage {
                     <hr/>
                     <div class="row py-1" style="border-top: 1px solid gray;">
                         <div class="col-2 col-md-1"><strong>{"Month"}</strong></div>
-                        <div class="col-6 col-md-3"><strong>{"Visiting Done"}</strong></div>
-                        <div class="col-4 col-md-2"><strong>{"Done"}</strong></div>
-                        <div class="col-8 col-md-3"><strong>{"Cooling Off"}</strong></div>
-                        <div class="col-4 col-md-2"><strong>{"Total"}</strong></div>
+                        //<div class="col-2 col-md-1"><strong>{"Visiting Done"}</strong></div>
+                        // <div class="col-2 col-md-1"><strong>{"Done"}</strong></div>
+                        // <div class="col-2 col-md-1"><strong>{"Cooling Off"}</strong></div>
+                        <div class="col-2 col-md-1"><strong>{"Completed"}</strong></div>
                     </div>
                     {
                         self.months.iter().map(|month| {   
@@ -97,16 +92,16 @@ impl Component for TerritoryStageReportPage {
                                          <div class="col-2 col-md-1" style="font-weight:bold;">
                                              {month.month.clone()}
                                          </div>
-                                         <div class="col-6 col-md-3">
-                                             {month.visiting_done}
-                                         </div>
-                                         <div class="col-4 col-md-2">
-                                            {month.done}
-                                        </div>
-                                        <div class="col-8 col-md-3">
-                                            {month.cooling_off}
-                                        </div>
-                                        <div class="col-4 col-md-2">
+                                        //  <div class="col-2 col-md-1">
+                                        //      {month.visiting_done}
+                                        //  </div>
+                                        //  <div class="col-2 col-md-1">
+                                        //     {month.done}
+                                        // </div>
+                                        // <div class="col-2 col-md-1">
+                                        //     {month.cooling_off}
+                                        // </div>
+                                        <div class="col-2 col-md-1">
                                             {month.total}
                                         </div>
                                     </div>
