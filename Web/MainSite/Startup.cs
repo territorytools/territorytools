5302cd0bbab016e9206175b5a9dcd3d2426a9d21
Marc Durham <marcdurham@gmail.com>
2023-10-25 20:54:13 -0700

    Disable new Google stuff

--- Web/MainSite/Startup.cs
+++ Web/MainSite/Startup.cs
@@ -93,33 +93,31 @@ namespace TerritoryTools.Web.MainSite
             {
                 //options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
             })
+            .AddGoogle(options =>
+            {
+                options.ClientId = Configuration["Authentication:Google:ClientId"];
+                options.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+            })
             //.AddGoogle(options =>
             //{
             //    options.ClientId = Configuration["Authentication:Google:ClientId"];
             //    options.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+            //    options.Scope.Add("profile");
+            //    options.Events.OnCreatingTicket = (context) =>
+            //    {
+            //        if(context.User.TryGetProperty("image", out JsonElement imageElement))
+            //        {
+            //            if(imageElement.TryGetProperty("url", out JsonElement urlElement))
+            //            {
+            //                string uri = urlElement.GetString();
+            //                //context.Identity.AddClaim(new Claim("image", context.User.GetValue("image").SelectToken("url").ToString()));
+            //                context.Identity.AddClaim(new Claim("image", uri));
+            //                Console.WriteLine($"Claim added to get user image URI: {uri}");
+            //            }
+            //        }
+            //        return Task.CompletedTask;
+            //    };
             //})
-            .AddGoogle(options =>
-            {
-                //options.ClientId = Configuration["Google.LoginProvider.ClientId"];
-                //options.ClientSecret = Configuration["Google.LoginProvider.ClientKey"];
-                options.ClientId = Configuration["Authentication:Google:ClientId"];
-                options.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
-                options.Scope.Add("profile");
-                options.Events.OnCreatingTicket = (context) =>
-                {
-                    if(context.User.TryGetProperty("image", out JsonElement imageElement))
-                    {
-                        if(imageElement.TryGetProperty("url", out JsonElement urlElement))
-                        {
-                            string uri = urlElement.GetString();
-                            //context.Identity.AddClaim(new Claim("image", context.User.GetValue("image").SelectToken("url").ToString()));
-                            context.Identity.AddClaim(new Claim("image", uri));
-                            Console.WriteLine($"Claim added to get user image URI: {uri}");
-                        }
-                    }
-                    return Task.CompletedTask;
-                };
-            })
             //.AddCookie(options =>
             //{
             //    options.ExpireTimeSpan = TimeSpan.FromDays(90);
