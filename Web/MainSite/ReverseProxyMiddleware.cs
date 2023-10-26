501a94309d2184fdbb36c4ae992d2c2c9ee5cfb0
Marc Durham <marcdurham@gmail.com>
2023-10-25 23:58:39 -0700

    Restore old copy of ReverseProxyMiddleware

--- Web/MainSite/ReverseProxyMiddleware.cs
+++ Web/MainSite/ReverseProxyMiddleware.cs
@@ -4,9 +4,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
 using System;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using TerritoryTools.Web.MainSite.Models;
 using TerritoryTools.Web.MainSite.Services;
@@ -45,63 +45,46 @@ namespace TerritoryTools.Web.MainSite
         public async Task Invoke(HttpContext context)
         {
             _logger.LogTrace($"ReverseProxy: Incoming: Path: {context.Request.Path} QueryString: {context.Request.QueryString}");
-            string isAuthMessage = $"{context?.User?.Identity?.IsAuthenticated ?? false}";
-            string userNameMessage = $"{context?.User?.Identity?.Name}";
-            string userAuthMessage = $"User IsAuthenticated: {isAuthMessage} Name: {userNameMessage}";
-            _logger.LogTrace(userAuthMessage);
-
             Uri targetUri = BuildTargetUri(context.Request);
-
-            // Security bypass Starts:  Comment this block out
-            //if (targetUri != null)
-            //{
-            //    HttpRequestMessage targetRequestMessage = CreateTargetMessage(context, targetUri);
-            //    using (HttpResponseMessage responseMessage = await _httpClientWrapper.SendAsync(targetRequestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted))
-            //    {
-            //        context.Response.StatusCode = (int)responseMessage.StatusCode;
-            //        CopyFromTargetResponseHeaders(context, responseMessage);
-            //        await responseMessage.Content.CopyToAsync(context.Response.Body);
-            //    }
-            //    return;
-            //}
-            //await _nextMiddleware(context);
-            //return;
-            // End of Security bypass.  Comment this block out
-
 
             if (targetUri != null)
             {
                 _logger.LogTrace($"ReverseProxy: Forwarded to: {targetUri}");
-                if(context.Request.Query.TryGetValue("mtk", out StringValues value) && !string.IsNullOrWhiteSpace(value))
+                if(context.Request.Query.TryGetValue("mtk", out StringValues value))
                 {
                     _logger.LogTrace($"MTK query param detected: {value}");
-                    TerritoryLinkContract link = _apiService.Get<TerritoryLinkContract>($"territory-links/{value}", "");
-                    if (link == null && !context.User.Identity.IsAuthenticated)
+                    if (string.IsNullOrWhiteSpace(value))
                     {
-                        string message = $"ReverseProxy: Territory key does not exist";
                         context.Response.StatusCode = 401;
-                        context.Response.Body = StreamFrom(message);
-                        _logger.LogTrace(message);
+                        _logger.LogTrace($"ReverseProxy: Territory key is missing");
                         return;
                     }
-                    else if ((!link.Successful || link.Expires != null && link.Expires < DateTime.UtcNow) 
-                        && !context.User.Identity.IsAuthenticated)
+                    else
                     {
-                        string message = $"ReverseProxy: Territory key expired {link.Expires}";
-                        context.Response.StatusCode = 403;
-                        context.Response.Body = StreamFrom(message);
-                        _logger.LogTrace(message);
-                        return;
+                        TerritoryLinkContract link = _apiService.Get<TerritoryLinkContract>($"territory-links/{value}", "");
+                        if (link == null)
+                        {
+                            context.Response.StatusCode = 401;
+                            _logger.LogTrace($"ReverseProxy: Territory key does not exist");
+                            return;
+                        }
+                        else if (!link.Successful || link.Expires != null && link.Expires < DateTime.UtcNow)
+                        {
+                            context.Response.StatusCode = 403;
+                            _logger.LogTrace($"ReverseProxy: Territory key expired {link.Expires}");
+                            return;
+                        }
+                        // TODO: Check from tt-api
+                        // It worked
+                        // TODO: Add more middle where to detect the header, or this mtk, or allow certain anonymous things in
                     }
                 }
                 else if (!context.User.Identity.IsAuthenticated)
                 {
                     //https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-context?view=aspnetcore-6.0#httpcontext-isnt-thread-safe
                     //HttpContext isn't thread safe
-                    string message = $"ReverseProxy: User Not Authenticated and MTK query param not detected";
                     context.Response.StatusCode = 401;
-                    context.Response.Body = StreamFrom(message);
-                    _logger.LogTrace(message);
+                    _logger.LogTrace($"ReverseProxy: Not Authenticated");
                     return;
                 }
 
@@ -109,7 +92,6 @@ namespace TerritoryTools.Web.MainSite
 
                 using (HttpResponseMessage responseMessage = await _httpClientWrapper.SendAsync(targetRequestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted))
                 {
-                    _logger.LogTrace($"HttpResponseMessage: StatusCode: {(int)responseMessage.StatusCode}");
                     context.Response.StatusCode = (int)responseMessage.StatusCode;
                     CopyFromTargetResponseHeaders(context, responseMessage);
                     await responseMessage.Content.CopyToAsync(context.Response.Body);
@@ -126,16 +108,6 @@ namespace TerritoryTools.Web.MainSite
             await _nextMiddleware(context);
         }
 
-        Stream StreamFrom(string text)
-        {
-            MemoryStream stream = new ();
-            StreamWriter writer = new (stream);
-            writer.Write(text);
-            writer.Flush();
-            stream.Position = 0;
-            return stream;
-        }
-
         private HttpRequestMessage CreateTargetMessage(HttpContext context, Uri targetUri)
         {
             var requestMessage = new HttpRequestMessage();
@@ -150,7 +122,7 @@ namespace TerritoryTools.Web.MainSite
 
         private void CopyFromOriginalRequestContentAndHeaders(HttpContext context, HttpRequestMessage requestMessage)
         {
-            string requestMethod = context.Request.Method;
+            var requestMethod = context.Request.Method;
 
             if (!HttpMethods.IsGet(requestMethod) &&
               !HttpMethods.IsHead(requestMethod) &&
@@ -161,37 +133,28 @@ namespace TerritoryTools.Web.MainSite
                 requestMessage.Content = streamContent;
             }
 
-            string headers = string.Empty;
             foreach (var header in context.Request.Headers)
             {
-                //_logger.LogTrace($"context.Request.Headers: {header.Key}: {string.Join(',', header.Value.ToArray())}");
-                headers += $"{header.Key}: {string.Join(',', header.Value.ToArray())}\n";
                 // Don't allow this header to be added by the client
                 if (!"x-territory-tools-user".Equals(header.Key, StringComparison.OrdinalIgnoreCase))
                 {
-                    requestMessage.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
-                }
-                else
-                {
-                    _logger.LogTrace($"Removing x-territory-tools-user header! Value: {string.Join(',', header.Value.ToArray())}");
+                    requestMessage.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
                 }
             }
 
-            _logger.LogTrace($"context.Request.Headers.List: \n{headers}\n(***User Header) {context.User.Identity.Name}");            
-            requestMessage.Headers.TryAddWithoutValidation("x-territory-tools-user", context.User.Identity.Name);
+            requestMessage.Headers.Add("x-territory-tools-user", context.User.Identity.Name);
+            //requestMessage.Content?.Headers.TryAddWithoutValidation("x-territory-tools-user", context.User.Identity.Name);
         }
 
         private void CopyFromTargetResponseHeaders(HttpContext context, HttpResponseMessage responseMessage)
         {
             foreach (var header in responseMessage.Headers)
             {
-                //_logger.LogTrace($"CopyFromTargetResponseHeaders: Headers: {header.Key}: {string.Join(',', header.Value.ToArray())}");
                 context.Response.Headers[header.Key] = header.Value.ToArray();
             }
 
             foreach (var header in responseMessage.Content.Headers)
             {
-                //_logger.LogTrace($"CopyFromTargetResponseHeaders: Content.Headers: {header.Key}: {string.Join(',', header.Value.ToArray())}");
                 context.Response.Headers[header.Key] = header.Value.ToArray();
             }
 
