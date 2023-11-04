0713afdd086589de38cc5eea561183401ffdd49f
Marc Durham <marcdurham@gmail.com>
2023-11-03 20:33:59 -0700

    The save posts, it works, no result.

--- Web/RustYewSpa/src/components/link_editor.rs
+++ Web/RustYewSpa/src/components/link_editor.rs
@@ -1,3 +1,4 @@
+use crate::components::button_with_confirm::ButtonWithConfirm;
 use crate::components::menu_bar_v2::MenuBarV2;
 use crate::components::menu_bar::MapPageLink;
 use crate::components::text_box::{InputCell, StringCell};
@@ -7,8 +8,9 @@ use crate::models::territory_links::{LinkChanges, TerritoryLinkContract};
 use crate::{field_string, http_get_set};
 use crate::field;
 
-use reqwasm::http::Request;
+use reqwasm::http::{Request, Method};
 use serde::{Serialize, Deserialize};
+use wasm_bindgen_futures::spawn_local;
 use std::ops::Deref;
 use wasm_bindgen::JsCast;
 use web_sys::HtmlInputElement;
@@ -50,86 +52,89 @@ pub fn user_editor_page() -> Html {
     let state: yew::UseStateHandle<LinkEditorModel> = use_state(LinkEditorModel::default);
     let location = use_location().expect("Should be a location to get query string");
     let parameters: LinkEditorParameters = location.query::<LinkEditorParameters>().expect("An object");
-    let link_id: String = match parameters.link_id { Some(v) => v, _ => "".to_string() };
+    
 
     let cloned_state = state.clone();
-    let link_id = link_id.clone();
-    // let save_onclick = Callback::from(move |_: i32| { 
-    //     //event.prevent_default();
-    //     let cloned_state = cloned_state.clone();
-    //     spawn_local(async move {
-    //         let uri_string: String = "/api/territory-links".to_string();
-    //         let uri: &str = uri_string.as_str();
-    //         let body_model = LinkSaveRequest {
-    //             created_by: Some("unkown".to_string()),
-    //             link: LinkChanges {
-    //                 id: cloned_state.link.id.clone(),
-    //                 territory_uri: cloned_state.link.territory_uri.clone(),
-    //                 alba_mobile_territory_key: cloned_state.link.alba_mobile_territory_key.clone(),
-    //                 territory_number: cloned_state.link.territory_number.clone(),
-    //                 territory_description: cloned_state.link.territory_description.clone(),
-    //                 created: cloned_state.link.created.clone(),
-    //                 expires: cloned_state.link.expires.clone(),
-    //                 expired: cloned_state.link.expired.clone(),
-    //                 assigned_date_utc: cloned_state.link.assigned_date_utc.clone(),
-    //                 assignee_name: cloned_state.link.assignee_name.clone(),
-    //                 assignee_email: cloned_state.link.assignee_email.clone(),
-    //                 assignee_phone: cloned_state.link.assignee_phone.clone(),
-    //                 territory_last_completed_by: cloned_state.link.territory_last_completed_by.clone(),
-    //                 territory_last_completed_date: cloned_state.link.territory_last_completed_date.clone(),
-    //                 stage_id: cloned_state.link.stage_id,
-    //                 successful: cloned_state.link.successful.clone(),
-    //             }
-    //         };
-    //         let data_serialized = serde_json::to_string_pretty(&body_model)
-    //             .expect("Should be able to serialize address for geocoding into JSON");
-    //         let method = if link_id.is_empty() { Method::POST } else { Method::PUT };
-    //         let resp = Request::new(uri)
-    //             .method(method)
-    //             .header("Content-Type", "application/json")
-    //             .body(data_serialized)
-    //             .send()
-    //             .await
-    //             .expect("A result from the endpoint");        
-    //         let _result = LinkSaveResult {
-    //             success: (resp.status() == 200),
-    //             errors: Some("".to_string()),
-    //             status: resp.status(),
-    //             completed: true,
-    //         };
-    //         //stage_change_result_state_clone.set(result);
-    //         let mut modified = cloned_state.deref().clone();
-    //         if resp.status() == 200 {
-    //             modified.save_success = true;
-    //             modified.save_error = false;
-    //             // UserSaveResult {
-    //             //     success: true,
-    //             //     errors: Some("".to_string()),
-    //             //     status: resp.status(),
-    //             //     completed: true,
-    //             // }
-    //         } else {
-    //             let errors = if (401..403).contains(&resp.status()) { 
-    //                 Some("Unauthorized".to_string()) 
-    //             } else {
-    //                 Some(resp.status().to_string())
-    //             };
-    //             modified.error_message = errors.unwrap_or_default();
-    //             modified.save_error = true;
-    //             modified.save_success = false;
-    //             // UserSaveResult {
-    //             //     success: false,
-    //             //     errors,
-    //             //     status: resp.status(),
-    //             //     completed: true,
-    //             // }
-    //         };
-    //         cloned_state.set(modified);
-    //     });
-    // });
+    let link_id: String = parameters.link_id.clone().unwrap_or_default(); // { Some(v) => v, _ => "".to_string() };
+    let save_onclick = Callback::from(move |_: i32| { 
+        //event.prevent_default();
+        let cloned_state = cloned_state.clone();
+        let link_id = link_id.clone();
+        spawn_local(async move {
+            let uri_string: String = "/api/territory-links".to_string();
+            let uri: &str = uri_string.as_str();
+            let body_model = LinkSaveRequest {
+                created_by: Some("unkown".to_string()),
+                link: LinkChanges {
+                    id: cloned_state.link.id.clone(),
+                    territory_uri: cloned_state.link.territory_uri.clone(),
+                    alba_mobile_territory_key: cloned_state.link.alba_mobile_territory_key.clone(),
+                    territory_number: cloned_state.link.territory_number.clone(),
+                    territory_description: cloned_state.link.territory_description.clone(),
+                    created: cloned_state.link.created.clone(),
+                    expires: cloned_state.link.expires.clone(),
+                    expired: cloned_state.link.expired,
+                    assigned_date_utc: cloned_state.link.assigned_date_utc.clone(),
+                    assignee_name: cloned_state.link.assignee_name.clone(),
+                    assignee_email: cloned_state.link.assignee_email.clone(),
+                    assignee_phone: cloned_state.link.assignee_phone.clone(),
+                    territory_last_completed_by: cloned_state.link.territory_last_completed_by.clone(),
+                    territory_last_completed_date: cloned_state.link.territory_last_completed_date.clone(),
+                    stage_id: cloned_state.link.stage_id,
+                    successful: cloned_state.link.successful,
+                }
+            };
+            let data_serialized = serde_json::to_string_pretty(&body_model)
+                .expect("Should be able to serialize address for geocoding into JSON");
+            let method = if link_id.is_empty() { Method::POST } else { Method::PUT };
+            let resp = Request::new(uri)
+                .method(method)
+                .header("Content-Type", "application/json")
+                .body(data_serialized)
+                .send()
+                .await
+                .expect("A result from the endpoint");        
+            let _result = LinkSaveResult {
+                success: (resp.status() == 200),
+                errors: Some("".to_string()),
+                status: resp.status(),
+                completed: true,
+            };
+            //stage_change_result_state_clone.set(result);
+            let mut modified = cloned_state.deref().clone();
+            if resp.status() == 200 {
+                modified.save_success = true;
+                modified.save_error = false;
+                // UserSaveResult {
+                //     success: true,
+                //     errors: Some("".to_string()),
+                //     status: resp.status(),
+                //     completed: true,
+                // }
+            } else {
+                let errors = if (401..403).contains(&resp.status()) { 
+                    Some("Unauthorized".to_string()) 
+                } else {
+                    Some(resp.status().to_string())
+                };
+                modified.error_message = errors.unwrap_or_default();
+                modified.save_error = true;
+                modified.save_success = false;
+                // UserSaveResult {
+                //     success: false,
+                //     errors,
+                //     status: resp.status(),
+                //     completed: true,
+                // }
+            };
+            cloned_state.set(modified);
+        });
+    });
+    
     //let parameters = parameters.clone();
     let cloned_state = state.clone();
-    let uri: String = format!("/api/territory-links/{link_id}");
+    let link_id: String = parameters.link_id.clone().unwrap_or_default(); 
+    let uri: String = format!("/api/territory-links/{}", link_id.clone());
     use_effect_with((), move |_| {
         http_get_set!(cloned_state.link, uri);
     });
@@ -165,9 +170,9 @@ pub fn user_editor_page() -> Html {
                 </div>
                 <div class="row g-3 my-2">
                     if true { //state.user_response.user_can_edit {
-                        // <div class="col-12 p-3">
-                        //     <ButtonWithConfirm id="save-button" button_text="Save" on_confirm={save_onclick.clone()} />
-                        // </div>
+                        <div class="col-12 p-3">
+                            <ButtonWithConfirm id="save-button" button_text="Save" on_confirm={save_onclick.clone()} />
+                        </div>
                     }
                 </div>
             </div>
